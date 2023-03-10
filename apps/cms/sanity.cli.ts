1ec4f395c999837f8876fd62bb4cfd0929a88af3
Ole Magnus Fon Johnsen <me@omfj.no>
2023-03-10 16:43:41 +0100

    wip: remove cms

--- apps/cms/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from "sanity/cli";
-
-export default defineCliConfig({
-  api: {
-    projectId: "pgq2pd26",
-    dataset: "production",
-  },
-});
--- apps/cms/src/resolveDocumentActions.js
+++
@@ -1,6 +0,0 @@
-import defaultResolve, { PublishAction } from 'part:@sanity/base/document-actions';
-import SetAndPublishAction from './setAndPublishAction';
-
-export default function resolveDocumentActions(props) {
-    return defaultResolve(props).map((Action) => (Action === PublishAction ? SetAndPublishAction : Action));
-}
