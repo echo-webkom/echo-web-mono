f612f935fb9de4342a35bdb8bd8b33307751173a
Ole Magnus Fon Johnsen <me@omfj.no>
2023-03-21 01:14:27 +0100

    fix: add renamed files git did not detect

---
+++ apps/web/src/components/markdown.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+interface MarkdownProps {
+  content: string;
+}
+
+export const Markdown = ({content}: MarkdownProps) => {
+  return (
+    <ReactMarkdown
+      className="prose md:prose-xl"
+      components={{
+        a: ({children, href}) => {
+          // TODO: Add external link icon and behavior
+          return (
+            <Link
+              className="transition-colors duration-200 after:content-['_↗'] hover:text-blue-500"
+              href={href ?? ""}
+            >
+              {children}
+            </Link>
+          );
+        },
+        img: ({src, alt}) => {
+          return (
+            <Image
+              src={src ?? ""}
+              alt={alt ?? ""}
+              width="600"
+              height="400"
+              className="mx-auto h-auto max-w-full"
+            />
+          );
+        },
+      }}
+      remarkPlugins={[remarkGfm]}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+};
