c1f0a7be4fac4b0c54a9c5b8ba39dae82c938088
elefantoft <KjetilAlvestad@gmail.com>
2023-03-22 17:40:33 +0100

    Create minutes api

---
+++ apps/web/src/api/minutes/schema.ts
@@ -0,0 +1,15 @@
+import {z} from "zod";
+
+export const minuteSchema = z.object({
+  date: z.string(),
+  allmote: z.boolean(),
+  title: z.string(),
+  document: z
+    .object({
+      asset: z.object({
+        url: z.string(),
+      }),
+    })
+    .transform((m) => m.asset.url),
+});
+export type Minute = z.infer<typeof minuteSchema>;
