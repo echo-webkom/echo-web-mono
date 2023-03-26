970add94e2087470dbf3bd151f35c0bbdbe581fd
elefantoft <KjetilAlvestad@gmail.com>
2023-03-26 21:25:00 +0200

    Update document query and schema

--- apps/web/src/api/minutes/schema.ts
+++ apps/web/src/api/minutes/schema.ts
@@ -5,12 +5,6 @@ export const minuteSchema = z.object({
   date: z.string(),
   allmote: z.boolean(),
   title: z.string(),
-  document: z
-    .object({
-      asset: z.object({
-        url: z.string(),
-      }),
-    })
-    .transform((m) => m.asset.url),
+  document: z.string(),
 });
 export type Minute = z.infer<typeof minuteSchema>;
