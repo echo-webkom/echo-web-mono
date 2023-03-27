1277a93b9fd854f8d7033eecf93205d9c3f9445c
elefantoft <KjetilAlvestad@gmail.com>
2023-03-27 15:39:32 +0200

    Remove dark color scheme from accordion

--- apps/web/src/components/accordion.tsx
+++ apps/web/src/components/accordion.tsx
@@ -11,7 +11,7 @@ const AccordionItem = React.forwardRef<
 >(({className, ...props}, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={classNames("border-b border-b-slate-200 dark:border-b-slate-700", className)}
+    className={classNames("border-b border-b-slate-200", className)}
     {...props}
   />
 ));
