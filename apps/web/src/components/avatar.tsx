7b6247b6338b7ac899f91bbb5edfa9c8f6a720bd
torgerbocianowski <bocianot@hu-berlin.de>
2023-03-30 16:38:02 +0200

    add rounded avatar images

--- apps/web/src/components/avatar.tsx
+++ apps/web/src/components/avatar.tsx
@@ -20,7 +20,7 @@ const AvatarImage = React.forwardRef<
 >(({className, ...props}, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn("h-full w-full rounded-full", className)}
     {...props}
   />
 ));
