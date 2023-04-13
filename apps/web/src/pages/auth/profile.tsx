4df50ad4762262c3f3384f5f6320ac9c5b6cdb5a
Ole Magnus Fon Johnsen <me@omfj.no>
2023-04-13 17:19:41 +0200

    Add WIP profile update

--- apps/web/src/pages/auth/profile.tsx
+++ apps/web/src/pages/auth/profile.tsx
@@ -4,19 +4,9 @@ import {type GetServerSideProps} from "next/types";
 
 import {getServerSession} from "@echo-webkom/auth";
 
-import Button from "@/components/button";
 import Container from "@/components/container";
-import {Input} from "@/components/input";
 import Layout from "@/components/layout";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/select";
+import UserForm from "@/components/user-form";
 import {api} from "@/utils/api";
 
 const ProfilePage = () => {
@@ -64,59 +54,11 @@ const ProfilePage = () => {
 
           <hr className="my-4" />
 
-          <form className="flex flex-col gap-5">
-            <div className="flex flex-col gap-2">
-              <Input placeholder="Alternativ e-post" />
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Velg årstrinn" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">1. trinn</SelectItem>
-                  <SelectItem value="2">2. trinn</SelectItem>
-                  <SelectItem value="3">3. trinn</SelectItem>
-                  <SelectItem value="4">4. trinn</SelectItem>
-                  <SelectItem value="5">5. trinn</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Velg studieretning" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Bachelor</SelectLabel>
-                    <SelectItem value="DSIK">Datasikkerhet</SelectItem>
-                    <SelectItem value="DTEK">Datateknologi</SelectItem>
-                    <SelectItem value="IMO">Informatikk-matematikk-økonomi</SelectItem>
-                    <SelectItem value="DVIT">Datavitenskap</SelectItem>
-                    <SelectItem value="BINF">Bioinformatikk</SelectItem>
-                  </SelectGroup>
-                  <SelectGroup>
-                    <SelectLabel>Master</SelectLabel>
-                    <SelectItem value="DSC">Master i datascience</SelectItem>
-                    <SelectItem value="INF">Master i informatikk</SelectItem>
-                    <SelectItem value="PROG">Programvareutvikling</SelectItem>
-                  </SelectGroup>
-                  <SelectGroup>
-                    <SelectLabel>Annet</SelectLabel>
-                    <SelectItem value="MISC">Annet</SelectItem>
-                    <SelectItem value="ARMNINF">Årstudium i informatikk</SelectItem>
-                    <SelectItem value="POST">Post-bachelor</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex gap-3">
-              <Button>Lagre</Button>
-            </div>
-          </form>
+          {user.data ? (
+            <UserForm user={user.data} refetchUser={() => user.refetch} />
+          ) : (
+            <p className="text-center text-lg text-neutral-500">Laster inn...</p>
+          )}
         </Container>
       </Layout>
     </>
