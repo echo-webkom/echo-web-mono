37eca698ac6734a177ffdd69c89c6f3d26cc8501
Torger Bocianowski <84389241+bocianowski1@users.noreply.github.com>
2023-04-16 16:20:13 +0200

    Add basic auth to sanity and middleware (#134)

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,24 @@
+import {NextResponse, type NextRequest} from "next/server";
+
+export const config = {
+  matcher: ["/api/sanity"],
+};
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    if (!authValue) {
+      return NextResponse.redirect("/");
+    }
+    const [user, password] = window.atob(authValue).split(":");
+
+    if (user?.toLowerCase() === "admin" && password === process.env.ADMIN_KEY) {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = "/";
+
+  return NextResponse.rewrite(url);
+}
