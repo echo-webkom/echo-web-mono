a95ca4e801a12d8d02eb270e918560502d433ad2
Ole Magnus Fon Johnsen <me@omfj.no>
2023-04-17 13:08:00 +0200

    Edge runtime compatible decode (#149)

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,8 @@
+import {Buffer} from "node:buffer";
 import {NextResponse, type NextRequest} from "next/server";
 
 export const config = {
+  runtime: "experimental-edge",
   matcher: ["/api/sanity"],
 };
 export function middleware(req: NextRequest) {
@@ -12,6 +14,7 @@ export function middleware(req: NextRequest) {
     if (!authValue) {
       return NextResponse.redirect("/");
     }
+
     const [user, password] = Buffer.from(authValue, "base64").toString().split(":");
 
     if (user?.toLowerCase() === "admin" && password === process.env.ADMIN_KEY) {
