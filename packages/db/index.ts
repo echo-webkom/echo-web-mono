85db0adb7f0541f3a7d42a462ef0e1206e3f439a
Ole Magnus Fon Johnsen <me@omfj.no>
2023-04-26 18:58:34 +0200

    anotottaaaa wan

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,10 +2,10 @@ import {PrismaClient} from "@prisma/client";
 
 export * from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {prisma: PrismaClient};
+const globalForPrisma = globalThis as {prisma?: PrismaClient};
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
