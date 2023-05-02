fa0ae1fb150a1d9c6239368302d083d967261134
Ole Magnus Fon Johnsen <me@omfj.no>
2023-05-02 21:38:56 +0200

    Use vercel postgres (#212)

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -3,13 +3,11 @@ import {z} from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     ADMIN_KEY: z.string().optional(),
   },
   client: {},
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     ADMIN_KEY: process.env.ADMIN_KEY,
   },
