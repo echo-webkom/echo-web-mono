e1ea8752dfa97e6e4420c95d87e83652914a4d81
Ole Magnus Fon Johnsen <me@omfj.no>
2023-05-20 14:16:31 +0200

    Hardcode providers

--- apps/web/src/app/(default)/auth/sign-in/page.tsx
+++ apps/web/src/app/(default)/auth/sign-in/page.tsx
@@ -1,21 +1,10 @@
-import {getProviders} from "next-auth/react";
-
 import Container from "@/components/container";
 import SignInButtons from "@/components/sign-in-buttons";
-import Heading from "@/components/ui/heading";
 
-export default async function SignInPage() {
-  const providers = await getProviders();
-
+export default function SignInPage() {
   return (
     <Container>
-      {providers ? (
-        <SignInButtons providers={providers} />
-      ) : (
-        <div className="flex flex-col justify-center gap-3">
-          <Heading>Ingen tilgjengelige innloggingstjenester</Heading>
-        </div>
-      )}
+      <SignInButtons />
     </Container>
   );
 }
--- apps/web/src/components/sign-in-buttons.tsx
+++ apps/web/src/components/sign-in-buttons.tsx
@@ -1,29 +1,33 @@
 "use client";
 
-import {signIn, type getProviders} from "next-auth/react";
+import {signIn} from "next-auth/react";
 
 import {Button} from "./ui/button";
 import Heading from "./ui/heading";
 
-export default function SignInButtons({
-  providers,
-}: {
-  providers: Exclude<Awaited<ReturnType<typeof getProviders>>, null>;
-}) {
+const providers = [
+  {
+    id: "feide",
+    name: "Feide",
+  },
+];
+
+export default function SignInButtons() {
   return (
     <div>
       <Heading className="text-center text-4xl">Velg en måte å logge inn på</Heading>
       <div className="my-10 flex flex-col justify-center gap-3">
-        {Object.values(providers).map((provider) => (
-          <div className="mx-auto" key={provider.name}>
+        {providers.map(({id, name}) => (
+          <div className="mx-auto" key={id}>
             <Button
-              onClick={() =>
-                void signIn(provider.id, {
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onClick={async () =>
+                signIn(id, {
                   callbackUrl: "/",
                 })
               }
             >
-              Logg inn med {provider.name}
+              Logg inn med {name}
             </Button>
           </div>
         ))}
