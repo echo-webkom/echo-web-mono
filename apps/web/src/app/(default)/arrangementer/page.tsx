080378bb2d9e9b9f4875f301cb8a680849558cfe
elefantoft <KjetilAlvestad@gmail.com>
2023-06-08 12:12:32 +0200

    Also fetch previous events and bedpresses

--- apps/web/src/app/(default)/arrangementer/page.tsx
+++ apps/web/src/app/(default)/arrangementer/page.tsx
@@ -1,19 +1,24 @@
 import Container from "@/components/container";
 import Events from "@/components/event-filter";
-import {fetchUpcomingBedpresses} from "@/sanity/bedpres";
-import {fetchComingEvents} from "@/sanity/event";
+import {$fetchAllBedpresses, type Bedpres} from "@/sanity/bedpres";
+import {$fetchAllEvents} from "@/sanity/event";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const [events, bedpresses] = await Promise.all([
-    fetchComingEvents(-1),
-    fetchUpcomingBedpresses(-1),
-  ]);
+  const [events, bedpresses] = await Promise.all([$fetchAllEvents(), $fetchAllBedpresses()]);
 
-  const e = events.map((event) => ({...event, type: "EVENT" as const}));
-  const b = bedpresses.map((bedpres) => ({...bedpres, type: "BEDPRES" as const}));
-  const happenings = [...e, ...b];
+  const happenings = [];
+
+  if (events instanceof Array<Event>) {
+    const e = events.map((event) => ({...event, type: "EVENT" as const}));
+    happenings.push(...e);
+  }
+
+  if (bedpresses instanceof Array<Bedpres>) {
+    const b = bedpresses.map((bedpres) => ({...bedpres, type: "BEDPRES" as const}));
+    happenings.push(...b);
+  }
 
   return (
     <Container className="grid grid-cols-1 gap-x-5 gap-y-12 px-3">
