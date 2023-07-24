766c2c5d8a2aa2ebd8ed8b4401e2751ad00d474a
Ole Magnus Fon Johnsen <me@omfj.no>
2023-07-24 14:16:57 +0200

    Add more docs

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type {Config} from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{tsx,mdx}", "./theme.config.tsx"],
   presets: [require("@echo-webkom/tailwind-config")],
 } satisfies Config;
