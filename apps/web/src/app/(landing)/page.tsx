0326a4f5e9019b4f29964d0798e26ff5e43b9e8f
Johanne <e-johher1@iskule.no>
2023-09-21 11:55:00 +0200

    update echo description

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -27,8 +27,8 @@ export default async function HomePage() {
                   </span>
                 </h1>
                 <p>
-                  Vi jobber utelukkende med å gjøre studiehverdagen for oss informatikere bedre og
-                  er studentenes stemme opp mot instituttet, fakultetet og arbeidsmarkedet.
+                  Vi i echo jobber med å gjøre studiehverdagen for oss informatikere bedre ved å
+                  arrangere sosiale og faglige arrangementer.
                 </p>
                 <div>
                   <Button asChild variant="secondary">
