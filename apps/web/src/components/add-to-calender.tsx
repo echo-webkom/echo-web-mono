90bf2f6d5a55e1f7bb58c673d5f4104e68977a2b
Zeno <zeleo1679@uib.no>
2023-10-04 18:36:26 +0200

    Added style ✨

--- apps/web/src/components/add-to-calender.tsx
+++ apps/web/src/components/add-to-calender.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as Dialog from '@radix-ui/react-dialog';
+import { Dialog, DialogContent, DialogTrigger, DialogTitle, DialogHeader } from "./ui/dialog";
 import { google, outlook, office365, yahoo, ics, type CalendarEvent } from "calendar-link";
 import Link from 'next/link';
 import test from 'node:test';
@@ -18,37 +18,31 @@ interface Props {
 
 export function AddToCalender({ date, title }: Props) {
     return (
-        <Dialog.Root>
-            <Dialog.Trigger>
-                <button>
-                    {date.toLocaleDateString("nb-NO")}
-                </button>
-            </Dialog.Trigger>
-            <Dialog.Portal>
-                <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                    <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-                        Legg til i kalender 📅
-                    </Dialog.Title>
-                    <Dialog.Content className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-                        <CalendarButton title={title} date={date} calendarType={CalendarType.GOOGLE} />
-                        <CalendarButton title={title} date={date} calendarType={CalendarType.OUTLOOK} />
-                        <CalendarButton title={title} date={date} calendarType={CalendarType.OFFICE} />
-                        <CalendarButton title={title} date={date} calendarType={CalendarType.YAHOO} />
-                        <CalendarButton title={title} date={date} calendarType={CalendarType.ICS} />
-                    </Dialog.Content>
+        <Dialog>
+            <DialogTrigger>
+                {date.toLocaleDateString("nb-NO")}
+            </DialogTrigger>
 
-                    <Dialog.Close asChild>
-                        <button
-                            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-                            aria-label="Close"
-                        >
-                            x
-                        </button>
-                    </Dialog.Close>
-                </Dialog.Content>
-            </Dialog.Portal>
-        </Dialog.Root>)
+
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="pl-5">
+                        Legg til i kalender 📅
+                    </DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-col sm:flex-row justify-around pt-4 gap-4 sm:gap-0">
+                    <CalendarButton title={title} date={date} calendarType={CalendarType.GOOGLE} />
+                    <CalendarButton title={title} date={date} calendarType={CalendarType.OUTLOOK} />
+                    <CalendarButton title={title} date={date} calendarType={CalendarType.OFFICE} />
+                    <CalendarButton title={title} date={date} calendarType={CalendarType.YAHOO} />
+                    <CalendarButton title={title} date={date} calendarType={CalendarType.ICS} />
+                </div>
+
+            </DialogContent>
+
+
+        </Dialog>)
 };
 
 enum CalendarType {
@@ -75,14 +69,21 @@ const CalendarButton = ({ title, date, calendarType }: CalendarButtonProps) => {
     }
     let calendarFunc = getCalendarFunc(calendarType)
     const link = calendarFunc(event)
+    const iconClassNames = "h-8 w-8"
     return (
-        <a href={link}>
-            {CalendarType.GOOGLE === calendarType && <BiLogoGoogle />}
-            {CalendarType.OUTLOOK === calendarType && <SiMicrosoftoutlook />}
-            {CalendarType.OFFICE === calendarType && <SiMicrosoftoffice />}
-            {CalendarType.YAHOO === calendarType && <SiYahoo />}
-            {CalendarType.ICS === calendarType && <FaFileDownload />}
-            <p>{calendarType}</p>
+        <a href={link} target="_blank">
+            <div className="flex flex-col sm:gap-4 py-3 sm:py-0 bg-gray-100 sm:bg-transparent rounded-sm">
+
+
+                <div className="mx-auto">
+                    {CalendarType.GOOGLE === calendarType && <BiLogoGoogle className={iconClassNames} />}
+                    {CalendarType.OUTLOOK === calendarType && <SiMicrosoftoutlook className={iconClassNames} />}
+                    {CalendarType.OFFICE === calendarType && <SiMicrosoftoffice className={iconClassNames} />}
+                    {CalendarType.YAHOO === calendarType && <SiYahoo className={iconClassNames} />}
+                    {CalendarType.ICS === calendarType && <FaFileDownload className={iconClassNames} />}
+                </div>
+                <p className="text-center">{calendarType}</p>
+            </div>
         </a>
 
     )
