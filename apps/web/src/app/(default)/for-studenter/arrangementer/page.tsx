14b9f63cdefd568046eba1e40314c2d03bf86cfc
elefantoft <KjetilAlvestad@gmail.com>
2023-10-13 17:46:33 +0200

    Change page from for-students to for-studenter, remove comment

--- apps/web/src/app/(default)/for-studenter/arrangementer/page.tsx
+++ apps/web/src/app/(default)/for-studenter/arrangementer/page.tsx
@@ -4,7 +4,6 @@ import Events from "@/components/event-filter";
 export default function HomePage() {
   return (
     <Container className="grid grid-cols-1 gap-x-5 gap-y-12 px-3">
-      {/* Events  */}
       <section className="lg:col-span-1"></section>
       <section className="flex flex-col gap-5 rounded-md border p-5">
         <Events />
