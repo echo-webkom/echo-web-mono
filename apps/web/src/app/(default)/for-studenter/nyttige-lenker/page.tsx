9ad849624ed03b37cb2f554e37f1ab05652e34ed
Herwal <herwal20@gmail.com>
2023-10-17 15:04:18 +0200

    Ga bedre link navn

--- apps/web/src/app/(default)/for-studenter/nyttige-lenker/page.tsx
+++ apps/web/src/app/(default)/for-studenter/nyttige-lenker/page.tsx
@@ -14,14 +14,14 @@ export default function NyttigeLenker() {
     <Container>
       <Heading>Nyttige lenker</Heading>
       <Link href={`https://www.youtube.com/watch?v=dQw4w9WgXcQ`}
-            target="_black"
+            target="_blank"
       >
       <div
         className={cn(
           "group flex h-full flex-col gap-3 rounded-lg p-5 shadow-lg hover:bg-muted"
         )}
       >
-        <h2 className="text-2xl font-bold">{"Nyttige lenke 1"}</h2>
+        <h2 className="text-2xl font-bold">{"Viktig info for nye elever"}</h2>
       </div>
     </Link>
     </Container>
