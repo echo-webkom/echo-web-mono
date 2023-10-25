f8d025b1f23a94843d141833f3312f56c0163dec
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-25 18:05:52 +0200

    started doing something

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { SiteHeader } from '@/components/site-header';
+import { Container } from '@/components/container';
 
 export default function NotFound() {
   const router = useRouter();
@@ -22,28 +24,31 @@ export default function NotFound() {
   });
 
   return (
-    <div className="font-block min-h-screen bg-[#000088] text-[#dddddd] selection:bg-gray-800/60">
-      <main className="mx-auto max-w-3xl space-y-10 px-4 py-20 text-xl md:text-3xl">
-        <h1 className="text-4xl md:text-6xl">Finner ikke siden du ser etter...</h1>
+    <Container className="relative pb-40 pt-24" layout="full">
+      {/* <SiteHeader /> */}
+      <div className="min-h-screen bg-[#d7d7da] text-primary selection:bg-gray-800/60">
+        <main className="mx-auto max-w-3xl space-y-10 px-4 py-20 text-xl md:text-xl">
+          <h1 className="text-3xl md:text-2xl">Finner ikke siden du ser etter...</h1>
 
-        <div className="space-y-4">
-          <p>Oops, ser ut som noen har rotet med koden her!</p>
+          <div className="space-y-4">
+            <p>Oops, ser ut som noen har rotet med koden her!</p>
 
-          <p>
-            Vi leter etter den savnede siden, men finner den ikke. Kanskje det er et tegn på at du
-            bør komme deg ut og strekke på bena? Ta en kort spasertur og nyt noen frisk luft, mens
-            vi prøver å fikse dette!
-          </p>
+            <p>
+              Vi leter etter den savnede siden, men finner den ikke. Kanskje det er et tegn på at du
+              bør komme deg ut og strekke på bena? Ta en kort spasertur og nyt noen frisk luft, mens
+              vi prøver å fikse dette!
+            </p>
 
-          <p>Trykk hvilken som helst tast for å gå tilbake til forsiden.</p>
-        </div>
+            <p>Trykk hvilken som helst tast for å gå tilbake til forsiden.</p>
+          </div>
 
-        <div className="text-center">
-          <Link href="/" className="p-4 hover:underline">
-            Klikk her for å gå tilbake til forsiden
-          </Link>
-        </div>
-      </main>
-    </div>
+          <div className="text-center">
+            <Link href="/" className="p-4 hover:underline">
+              Klikk her for å gå tilbake til forsiden
+            </Link>
+          </div>
+        </main>
+      </div>
+    </Container>
   );
 }
