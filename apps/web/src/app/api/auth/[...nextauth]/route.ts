a2c7c3b7daf16b5e63f6e8290b7445967e37c014
Ole Magnus Fon Johnsen <me@omfj.no>
2023-10-28 15:17:22 +0200

    Upgrade next (#824)

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "@echo-webkom/auth";
 
-// FIXME: WTF?
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
 
