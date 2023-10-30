e68a346a89f03e8ef28ff30db857867d8ce4ebbc
nikolaus <Nikolaus.Engh@student.uib.no>
2023-10-30 17:24:45 +0100

    Link to event dashboard

---
+++ apps/web/src/lib/queries/student-groups.ts
@@ -0,0 +1,10 @@
+import { eq } from "drizzle-orm";
+
+import { db } from "@echo-webkom/db";
+import { type User } from "@echo-webkom/db/schemas";
+
+export async function getUserStudentGroups(userId: User["id"]) {
+  return await db.query.usersToGroups.findMany({
+    where: (userToGroup) => eq(userToGroup.userId, userId)
+  });
+}
