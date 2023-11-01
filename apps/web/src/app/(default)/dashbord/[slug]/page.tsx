b20cba97a5897e48a199cc2f024fe2d7bce18332
Jonas Hammerseth <jonashammerseth@hotmail.com>
2023-11-01 13:55:34 +0100

    added happening info box to event dashboard

--- apps/web/src/app/(default)/dashbord/[slug]/page.tsx
+++ apps/web/src/app/(default)/dashbord/[slug]/page.tsx
@@ -3,18 +3,11 @@ import { notFound } from "next/navigation";
 import { eq } from "drizzle-orm";
 
 import { db } from "@echo-webkom/db";
-import { RegistrationStatus, type Group, type Registration, type User } from "@echo-webkom/db/schemas";
-import { registrationStatusToString } from "@echo-webkom/lib";
 
 import { Container } from "@/components/container";
-import { Heading } from "@/components/typography/heading";
-import { Button } from "@/components/ui/button";
+import { HappeningInfoBox } from "@/components/happening-info-box";
+import { RegistrationTable } from "@/components/registration-table";
 import { getHappeningBySlug } from "@/lib/queries/happening";
-
-
-import { cn } from "@/utils/cn";
-import { EditRegistrationButton } from "@/components/edit-registration-button";
-import { HappeningPreviewBox } from "@/components/happening-preview-box";
 
 type Props = {
   params: {
@@ -46,6 +39,8 @@ export default async function EventDashboard({ params }: Props) {
     },
   });
 
+  const happeningType = happening.type === "event" ? "arrangement" : "bedpres";
+
   const registered = registrations.filter((registration) => registration.status === "registered");
   const waitlist = registrations.filter((registration) => registration.status === "waiting");
   const unregistered = registrations.filter(
@@ -55,15 +50,13 @@ export default async function EventDashboard({ params }: Props) {
 
   return (
     <Container className="flex flex-col gap-10">
-      <Heading>
-        Dashboard:{" "}
-
-        <Link className="hover:underline" href={"/event/" + slug}>
-          {happening.title}
+      <div className="m-2">
+        <Link href={`/${happeningType}/${happening.slug}`}>
+          <span className="p-2">⇐</span>
+          <span className="underline">Tilbake</span>
           {eventInfo.type === "BEDPRES" && " (Bedriftspresentasjon)"}
         </Link>
-      </Heading>
-
+      </div>
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
         <div className="rounded-xl border px-3 py-8 text-center">
           <p>Antall påmeldte</p>
@@ -86,111 +79,14 @@ export default async function EventDashboard({ params }: Props) {
           <p className="text-7xl">{removed.length}</p>
         </div>
       </div>
+
+      <div className="flex flex-col gap-3">
+        <HappeningInfoBox happening={happening} />
+      </div>
       <div className="flex flex-col gap-3">
         <h2 className="text-3xl font-semibold">Registrerte</h2>
         <RegistrationTable registrations={registrations} />
       </div>
     </Container>
   );
-}
-
-export type RegistrationWithUser = Omit<Registration, "userId"> & {
-  user: User & {
-    memberships: Array<{
-      group: Group | null;
-    }>;
-  };
-};
-
-function RegistrationTable({ registrations }: { registrations: Array<RegistrationWithUser> }) {
-
-  return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-left text-sm text-gray-500 table-auto">
-        <thead className="bg-gray-200 text-xs uppercase">
-          <tr>
-            <th scope="col" className="px-6 py-4 text-left">
-              Navn
-            </th>
-            <th scope="col" className="px-6 py-4 text-left hidden md:table-cell">
-              E-post
-            </th>
-            <th scope="col" className="px-6 py-4 text-left">
-              Status
-            </th>
-            <th scope="col" className="px-6 py-4 text-left hidden md:table-cell">
-              Grunn
-            </th>
-            <th scope="col" className="px-6 py-4 text-left hidden md:table-cell">
-              Undergrupper
-            </th>
-            <th scope="col" className="px-6 py-4 text-left">
-              Handling
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {registrations.map((registration, i) => (
-            <RegistrationRow key={registration.userId} registration={registration} index={i} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
-// The rest of your code, including the EventDashboard component, remains the same.
-
-const RegistrationRow = ({
-  registration,
-  index,
-}: {
-  registration: RegistrationWithUser;
-  index: number;
-}) => {
-  const email = registration.user.alternativeEmail ?? registration.user.email ?? "";
-  const slug = registration.happeningSlug;
-  const statusClass = getStatusClassColor(registration.status);
-
-  return (
-    <tr
-      key={registration.user.id}
-      className={cn("border-b", {
-        "bg-white": index % 2 === 0,
-      })}
-    >
-      <th scope="row" className="whitespace-nowrap px-6 py-4 font-medium">
-        {registration.user.name}
-      </th>
-      <td className="px-6 py-4 hidden md:table-cell">
-        <Link className="hover:underline" href={"mailto:" + email}>
-          {email}
-        </Link>
-      </td>
-      <td className={`px-6 py-4`}><span className={`${statusClass}`}>{registrationStatusToString[registration.status]}</span></td>
-      <td className="px-6 py-4">{registration.unregisterReason}</td>
-      <td className="px-6 py-4">
-        {registration.user.memberships.map((membership) => membership.group?.name).join(", ")}
-        {registration.user.memberships.length === 0 && "Ingen"}
-      </td>
-      <td className="px-6 py-4">
-      <EditRegistrationButton slug={slug} registration={registration}/>
-      </td>
-    </tr>
-  );
-};
-
-function getStatusClassColor(status: RegistrationStatus): string {
-  switch (status) {
-    case "registered":
-      return "text-green-600";
-    case "waiting":
-      return "text-yellow-600";
-    case "unregistered":
-      return "text-red-600";
-    case "removed":
-      return "text-red-600";
-    default:
-      return "";
-  }
 }
---
+++ apps/web/src/components/happening-info-box.tsx
@@ -0,0 +1,173 @@
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { isAfter, isBefore } from "date-fns";
+
+import { Happening } from "@echo-webkom/db/schemas";
+
+import { Heading } from "@/components/ui/heading";
+import { maxCapacityBySlug } from "@/lib/queries/happening";
+import { fetchBedpresBySlug, type Bedpres } from "@/sanity/bedpres";
+import { fetchEventBySlug, type Event } from "@/sanity/event";
+import { cn } from "@/utils/cn";
+import { urlFor } from "@/utils/image-builder";
+import { capitalize } from "@/utils/string";
+
+export async function HappeningInfoBox({ happening }: { happening: Happening }) {
+  return (
+    <div>
+      {happening.type === "event" && <EventInfoBox event={await getEventBySlug(happening.slug)} />}
+      {happening.type === "bedpres" && (
+        <BedpresInfoBox bedpres={await getBedpresBySlug(happening.slug)} />
+      )}
+    </div>
+  );
+}
+
+async function getBedpresBySlug(slug: string) {
+  const bedpres = await fetchBedpresBySlug(slug);
+
+  if (!bedpres) {
+    return notFound();
+  }
+
+  return bedpres;
+}
+
+async function getEventBySlug(slug: string) {
+  const happening = await fetchEventBySlug(slug);
+
+  if (!happening) {
+    return notFound();
+  }
+
+  return happening;
+}
+
+async function EventInfoBox({ event }: { event: Event }) {
+  const isRegistrationOpen =
+    event?.registrationStart &&
+    event?.registrationEnd &&
+    isAfter(new Date(), new Date(event.registrationStart)) &&
+    isBefore(new Date(), new Date(event.registrationEnd));
+
+  return (
+    <div
+      className={cn(
+        "flex",
+        "h-full",
+        "items-center",
+        "rounded-xl",
+        "border",
+        "gap-5",
+        "p-5",
+        "bg-card",
+        "overflow-x-auto",
+        "sm:rounded-lg",
+      )}
+    >
+      <div className="overflow-x-hidden">
+        <Heading className="line-clamp-1 text-lg font-semibold md:text-2xl">
+          {capitalize(event.title)}
+        </Heading>
+        <p className="text-sm md:text-base">Arrangement</p>
+
+        <div className="py-4">
+          <div>Sted: {event.location?.name ? event.location.name : "Ikke bestemt"}</div>
+          <div>Kapasitet: {await maxCapacityBySlug(event.slug) === 0 ? 'uendelig' : await maxCapacityBySlug(event.slug)}</div>
+          <div>Publisert: {new Date(event._createdAt).toLocaleDateString()}</div>
+          <div>Sist oppdatert: {new Date(event._updatedAt).toLocaleDateString()}</div>
+          <div>
+            Kontakt:{" "}
+            {event.contacts && event.contacts.length > 0 ? event.contacts[0]?.email : "Ikke angitt"}
+          </div>
+          <div>
+            Påmelding:{" "}
+            {isRegistrationOpen ? (
+              <span className="text-green-600">Åpen</span>
+            ) : (
+              <span className="text-red-600">Lukket</span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+async function BedpresInfoBox({ bedpres }: { bedpres: Bedpres }) {
+  const isRegistrationOpen =
+    bedpres?.registrationStart &&
+    bedpres?.registrationEnd &&
+    isAfter(new Date(), new Date(bedpres.registrationStart)) &&
+    isBefore(new Date(), new Date(bedpres.registrationEnd));
+
+  return (
+    <div
+      className={cn(
+        "flex",
+        "h-full",
+        "items-center",
+        "gap-5",
+        "p-5",
+        "bg-card",
+        "border",
+        "overflow-x-auto",
+        "sm:rounded-lg",
+      )}
+    >
+      <div className="flex-1">
+        {!bedpres && (
+          <div className="border-l-4 border-yellow-500 bg-wave p-4 text-yellow-700">
+            <p className="font-semibold">Fant ikke arrangementet.</p>
+            <p>Kontakt Webkom!</p>
+          </div>
+        )}
+
+        <Heading className="line-clamp-1 text-lg font-semibold md:text-2xl">
+          {capitalize(bedpres!.title)}
+        </Heading>
+
+        <Link href={bedpres.company.website} className="hover:underline">
+          Bedriftspresentasjon {bedpres.company.name}
+          <ExternalLinkIcon className="ml-1 inline-block h-4 w-4" />
+        </Link>
+
+        <div className="py-4">
+          <div>Sted: {bedpres.location?.name ? bedpres.location.name : "Ikke bestemt"}</div>
+          <div>Kapasitet: {await maxCapacityBySlug(bedpres.slug) === 0 ? 'uendelig' : await maxCapacityBySlug(bedpres.slug)}</div>
+          <div>Publisert: {new Date(bedpres._createdAt).toLocaleDateString()}</div>
+          <div>Sist oppdatert: {new Date(bedpres._updatedAt).toLocaleDateString()}</div>
+          <div>
+            Kontakt:{" "}
+            {bedpres.contacts && bedpres.contacts.length > 0
+              ? bedpres.contacts[0]?.email
+              : "Ikke angitt"}
+          </div>
+          <div>
+            Påmelding:{" "}
+            {isRegistrationOpen ? (
+              <span className="text-green-600">Åpen</span>
+            ) : (
+              <span className="text-red-600">Lukket</span>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="invisible flex w-1/3 justify-end md:visible">
+        <div className="overflow-hidden">
+          <div className="relative aspect-square">
+            <Image
+              src={urlFor(bedpres.company.image).url()}
+              alt={`${bedpres.company.name} logo`}
+              width={10}
+              height={10}
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
