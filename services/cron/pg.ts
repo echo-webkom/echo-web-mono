13070660395e3b5306aea1352ce930b043808c49
Ole Magnus Fon Johnsen <me@omfj.no>
2023-11-23 15:01:11 +0100

    add run query helper

--- services/cron/pg.ts
+++ services/cron/pg.ts
@@ -6,3 +6,10 @@ await loadEnv();
 export const pg = new Client(
   Deno.env.get("DATABASE_URL"),
 );
+
+export const runQuery = async (query: string) => {
+  await pg.connect();
+  const result = await pg.queryArray(query);
+  await pg.end();
+  return result;
+};
