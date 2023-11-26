a2aba3c37a8bbc0c82f5b8876cf44c9b771301a8
Ole Magnus Fon Johnsen <me@omfj.no>
2023-11-26 14:37:08 +0100

    add comments

--- services/cron/main.ts
+++ services/cron/main.ts
@@ -4,18 +4,27 @@ import {
   handleResetYear,
 } from "./handlers.ts";
 
+/**
+ * Runs at 00:00 on the 1st of January and July
+ */
 Deno.cron(
   "delete sensitive data",
   "0 0 1 1,7 *",
   handleDeleteSensitiveQuestions,
 );
 
+/**
+ * Runs at 00:00 on the 1st of January and July
+ */
 Deno.cron(
   "delete old strikes",
   "0 0 1 1,7 *",
   handleDeleteOldStrikes,
 );
 
+/**
+ * Runs at 00:00 on the 1st of July
+ */
 Deno.cron(
   "reset user year",
   "0 0 1 7 *",
