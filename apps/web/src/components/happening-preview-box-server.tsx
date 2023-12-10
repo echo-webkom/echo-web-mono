e02b0b9146f14410e0d888925ef8d6beb5fbcbe2
Johanne <joher2128@uib.no>
2023-12-10 22:14:09 +0100

    preview for server components

---
+++ apps/web/src/components/happening-preview-box-server.tsx
@@ -0,0 +1,178 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRightIcon, CalendarIcon } from "@radix-ui/react-icons";
+import { format, isAfter, isBefore, isToday } from "date-fns";
+import nb from "date-fns/locale/nb";
+import { eq } from "drizzle-orm";
+
+import { db } from "@echo-webkom/db";
+import { type Registration } from "@echo-webkom/db/schemas";
+
+import { type Happening, type HappeningType } from "@/sanity/happening/schemas";
+import { cn } from "@/utils/cn";
+import { urlFor } from "@/utils/image-builder";
+import { capitalize } from "@/utils/string";
+
+const happeningTypeToString: Record<HappeningType, string> = {
+  event: "arrangementer",
+  bedpres: "bedriftspresentasjoner",
+};
+
+const typeToLink: Record<HappeningType, string> = {
+  event: "/for-studenter/arrangementer?type=arrangement",
+  bedpres: "/for-studenter/arrangementer?type=bedpres",
+};
+
+type HappeningPreviewBoxProps = {
+  type: HappeningType;
+  happenings: Array<Happening>;
+};
+
+const getSpotRangeInfo = <TSpotRange extends { spots: number; minYear: number; maxYear: number }>(
+  spotRanges: Array<TSpotRange>,
+  registrations: Array<Registration>,
+) => {
+  const maxCapacity = spotRanges.reduce((acc, curr) => acc + curr.spots, 0);
+  const registeredCount = registrations.filter(
+    (registration) => registration.status === "registered",
+  ).length;
+  return {
+    maxCapacity,
+    registeredCount,
+  };
+};
+
+export function HappeningPreviewBox({ type, happenings }: HappeningPreviewBoxProps) {
+  return (
+    <div>
+      <Link href={typeToLink[type]}>
+        <h2 className="group text-center text-xl font-semibold decoration-1 underline-offset-8 hover:underline md:text-3xl">
+          {capitalize(happeningTypeToString[type])}
+          <ArrowRightIcon className="ml-2 inline h-4 w-4 transition-transform group-hover:translate-x-2" />
+        </h2>
+      </Link>
+      <hr className="my-3" />
+
+      {happenings.length > 0 ? (
+        <ul className="flex h-full flex-col divide-y  ">
+          {happenings.map((happening) => (
+            <li key={happening._id} className="h-28 py-3">
+              {type === "event" && <EventPreview event={happening} />}
+              {type === "bedpres" && <BedpresPreview bedpres={happening} />}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="px-3 py-5 text-center text-lg font-medium">
+          <p>Ingen kommende {happeningTypeToString[type]}</p>
+          {/* TODO: Add funny gif */}
+        </div>
+      )}
+    </div>
+  );
+}
+
+type EventPreviewProps = {
+  event: Happening;
+};
+
+export async function EventPreview({ event }: EventPreviewProps) {
+  const spotRanges = event.spotRanges;
+  const registrations = await db.query.registrations.findMany({
+    where: (registration) => eq(registration.happeningId, event._id),
+    with: {
+      user: true,
+    },
+  });
+  const { maxCapacity, registeredCount } = getSpotRangeInfo(spotRanges ?? [], registrations);
+  const registrationStatus = (maxCapacity: number, registeredCount: number) => {
+    if (!event.registrationStart) {
+      return null;
+    }
+    if (isToday(new Date()) && isBefore(new Date(), new Date(event.registrationStart))) {
+      return "Påmelding i dag";
+    } else if (isBefore(new Date(), new Date(event.registrationStart))) {
+      return "Påmelding: " + format(new Date(event.registrationStart), "dd. MMM", { locale: nb });
+    } else if (isAfter(new Date(), new Date(event.registrationStart))) {
+      return registeredCount + "/" + (maxCapacity || ("Uendelig" && "∞"));
+    }
+    return "Fullt";
+  };
+  return (
+    <Link href={`/arrangement/${event.slug}`}>
+      <div className={cn("flex h-full items-center gap-5 p-5", "hover:bg-muted")}>
+        <div className="flex w-full justify-between overflow-x-hidden">
+          <h3 className="text-md my-auto line-clamp-1 font-semibold sm:text-2xl">{event.title}</h3>
+          <ul className="text-sm md:text-base">
+            {event.date && (
+              <li className="flex w-28 justify-end">
+                <CalendarIcon className="my-auto mr-1" />
+                {format(new Date(event.date), "dd. MMM", { locale: nb })}
+              </li>
+            )}
+            <li className="flex justify-end">{registrationStatus(maxCapacity, registeredCount)}</li>
+          </ul>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+type BedpresPreviewProps = {
+  bedpres: Happening;
+};
+
+export async function BedpresPreview({ bedpres }: BedpresPreviewProps) {
+  const spotRanges = bedpres.spotRanges;
+  const registrations = await db.query.registrations.findMany({
+    where: (registration) => eq(registration.happeningId, bedpres._id),
+    with: {
+      user: true,
+    },
+  });
+  const { maxCapacity, registeredCount } = getSpotRangeInfo(spotRanges ?? [], registrations);
+  const registrationStatus = (maxCapacity: number, registeredCount: number) => {
+    if (!bedpres.registrationStart) {
+      return null;
+    }
+    if (isToday(new Date()) && isBefore(new Date(), new Date(bedpres.registrationStart))) {
+      return "Påmelding i dag";
+    } else if (isBefore(new Date(), new Date(bedpres.registrationStart))) {
+      return "Påmelding: " + format(new Date(bedpres.registrationStart), "dd. MMM", { locale: nb });
+    } else if (isAfter(new Date(), new Date(bedpres.registrationStart))) {
+      return registeredCount + "/" + (maxCapacity || ("Uendelig" && "∞"));
+    }
+    return "Fullt";
+  };
+  return (
+    <Link href={`/bedpres/${bedpres.slug}`}>
+      <div className={cn("flex h-full items-center gap-5 p-3", "hover:bg-muted")}>
+        <div className="overflow-hidden rounded-full border">
+          <div className="relative aspect-square h-16 w-16">
+            {bedpres.company && (
+              <Image
+                src={urlFor(bedpres.company.image).url()}
+                alt={`${bedpres.company.name} logo`}
+                fill
+              />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between overflow-x-hidden">
+          <h3 className="my-auto line-clamp-1 text-lg font-semibold md:text-2xl">
+            {bedpres.title}
+          </h3>
+          <ul className="text-sm md:text-base">
+            {bedpres.date && (
+              <li className="flex justify-end">
+                <CalendarIcon className="my-auto mr-1" />
+                {format(new Date(bedpres.date), "dd. MMM", { locale: nb })}
+              </li>
+            )}
+            <li className="flex justify-end">{registrationStatus(maxCapacity, registeredCount)}</li>
+          </ul>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- apps/web/src/components/happening-preview-box.tsx
+++ apps/web/src/components/happening-preview-box.tsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowRightIcon, CalendarIcon } from "@radix-ui/react-icons";
-import { format, isAfter, isBefore, isToday } from "date-fns";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
 import nb from "date-fns/locale/nb";
-import { eq } from "drizzle-orm";
-
-import { db } from "@echo-webkom/db";
-import { type Registration } from "@echo-webkom/db/schemas";
 
 import { type Happening, type HappeningType } from "@/sanity/happening/schemas";
 import { cn } from "@/utils/cn";
@@ -28,20 +24,6 @@ type HappeningPreviewBoxProps = {
   happenings: Array<Happening>;
 };
 
-const getSpotRangeInfo = <TSpotRange extends { spots: number; minYear: number; maxYear: number }>(
-  spotRanges: Array<TSpotRange>,
-  registrations: Array<Registration>,
-) => {
-  const maxCapacity = spotRanges.reduce((acc, curr) => acc + curr.spots, 0);
-  const registeredCount = registrations.filter(
-    (registration) => registration.status === "registered",
-  ).length;
-  return {
-    maxCapacity,
-    registeredCount,
-  };
-};
-
 export function HappeningPreviewBox({ type, happenings }: HappeningPreviewBoxProps) {
   return (
     <div>
@@ -54,9 +36,9 @@ export function HappeningPreviewBox({ type, happenings }: HappeningPreviewBoxPro
       <hr className="my-3" />
 
       {happenings.length > 0 ? (
-        <ul className="flex h-full flex-col divide-y  ">
+        <ul className="flex h-full flex-col divide-y">
           {happenings.map((happening) => (
-            <li key={happening._id} className="h-28 py-3">
+            <li key={happening._id} className="py-3">
               {type === "event" && <EventPreview event={happening} />}
               {type === "bedpres" && <BedpresPreview bedpres={happening} />}
             </li>
@@ -76,43 +58,31 @@ type EventPreviewProps = {
   event: Happening;
 };
 
-export async function EventPreview({ event }: EventPreviewProps) {
-  const spotRanges = event.spotRanges;
-  const registrations = await db.query.registrations.findMany({
-    where: (registration) => eq(registration.happeningId, event._id),
-    with: {
-      user: true,
-    },
-  });
-  const { maxCapacity, registeredCount } = getSpotRangeInfo(spotRanges ?? [], registrations);
-  const registrationStatus = (maxCapacity: number, registeredCount: number) => {
-    if (!event.registrationStart) {
-      return null;
-    }
-    if (isToday(new Date()) && isBefore(new Date(), new Date(event.registrationStart))) {
-      return "Påmelding i dag";
-    } else if (isBefore(new Date(), new Date(event.registrationStart))) {
-      return "Påmelding: " + format(new Date(event.registrationStart), "dd. MMM", { locale: nb });
-    } else if (isAfter(new Date(), new Date(event.registrationStart))) {
-      return registeredCount + "/" + (maxCapacity || ("Uendelig" && "∞"));
-    }
-    return "Fullt";
-  };
+export function EventPreview({ event }: EventPreviewProps) {
   return (
     <Link href={`/arrangement/${event.slug}`}>
       <div className={cn("flex h-full items-center gap-5 p-5", "hover:bg-muted")}>
-        <div className="flex w-full justify-between overflow-x-hidden">
-          <h3 className="text-md my-auto line-clamp-1 text-lg font-semibold sm:text-2xl">
-            {event.title}
-          </h3>
+        <div className="overflow-x-hidden">
+          <h3 className="line-clamp-1 text-lg font-semibold md:text-2xl">{event.title}</h3>
           <ul className="text-sm md:text-base">
+            <li>
+              <span className="font-semibold">Gruppe:</span>{" "}
+              {capitalize(event.organizers.map((o) => o.name).join(", "))}
+            </li>
             {event.date && (
-              <li className="flex justify-end">
-                <CalendarIcon className="my-auto mr-1" />
-                {format(new Date(event.date), "dd. MMM", { locale: nb })}
+              <li>
+                <span className="font-semibold">Dato:</span>{" "}
+                {format(new Date(event.date), "d. MMMM yyyy", { locale: nb })}
               </li>
             )}
-            <li className="flex justify-end">{registrationStatus(maxCapacity, registeredCount)}</li>
+            <li>
+              <span className="font-semibold">Påmelding:</span>{" "}
+              {event.registrationStart
+                ? format(new Date(event.registrationStart), "d. MMMM yyyy", {
+                    locale: nb,
+                  })
+                : "Påmelding åpner snart"}
+            </li>
           </ul>
         </div>
       </div>
@@ -124,33 +94,12 @@ type BedpresPreviewProps = {
   bedpres: Happening;
 };
 
-export async function BedpresPreview({ bedpres }: BedpresPreviewProps) {
-  const spotRanges = bedpres.spotRanges;
-  const registrations = await db.query.registrations.findMany({
-    where: (registration) => eq(registration.happeningId, bedpres._id),
-    with: {
-      user: true,
-    },
-  });
-  const { maxCapacity, registeredCount } = getSpotRangeInfo(spotRanges ?? [], registrations);
-  const registrationStatus = (maxCapacity: number, registeredCount: number) => {
-    if (!bedpres.registrationStart) {
-      return null;
-    }
-    if (isToday(new Date()) && isBefore(new Date(), new Date(bedpres.registrationStart))) {
-      return "Påmelding i dag";
-    } else if (isBefore(new Date(), new Date(bedpres.registrationStart))) {
-      return "Påmelding: " + format(new Date(bedpres.registrationStart), "dd. MMM", { locale: nb });
-    } else if (isAfter(new Date(), new Date(bedpres.registrationStart))) {
-      return registeredCount + "/" + (maxCapacity || ("Uendelig" && "∞"));
-    }
-    return "Fullt";
-  };
+export function BedpresPreview({ bedpres }: BedpresPreviewProps) {
   return (
     <Link href={`/bedpres/${bedpres.slug}`}>
-      <div className={cn("flex h-full items-center gap-5 p-3", "hover:bg-muted")}>
+      <div className={cn("flex h-full items-center gap-5 p-5", "hover:bg-muted")}>
         <div className="overflow-hidden rounded-full border">
-          <div className="relative aspect-square h-16 w-16">
+          <div className="relative aspect-square h-20 w-20">
             {bedpres.company && (
               <Image
                 src={urlFor(bedpres.company.image).url()}
@@ -160,18 +109,23 @@ export async function BedpresPreview({ bedpres }: BedpresPreviewProps) {
             )}
           </div>
         </div>
-        <div className="flex flex-1 justify-between overflow-x-hidden">
-          <h3 className="my-auto line-clamp-1 text-lg font-semibold md:text-2xl">
-            {bedpres.title}
-          </h3>
+        <div className="overflow-x-hidden">
+          <h3 className="line-clamp-1 text-lg font-semibold md:text-2xl">{bedpres.title}</h3>
           <ul className="text-sm md:text-base">
             {bedpres.date && (
-              <li className="flex justify-end">
-                <CalendarIcon className="my-auto mr-1" />
-                {format(new Date(bedpres.date), "dd. MMM", { locale: nb })}
+              <li>
+                <span className="font-semibold">Dato:</span>{" "}
+                {format(new Date(bedpres.date), "d. MMMM yyyy", { locale: nb })}
               </li>
             )}
-            <li className="flex justify-end">{registrationStatus(maxCapacity, registeredCount)}</li>
+            <li>
+              <span className="font-semibold">Påmelding:</span>{" "}
+              {bedpres.registrationStart
+                ? format(new Date(bedpres.registrationStart), "d. MMMM yyyy", {
+                    locale: nb,
+                  })
+                : "Påmelding åpner snart"}
+            </li>
           </ul>
         </div>
       </div>
