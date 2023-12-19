aeac69b7985d78c1404629a216cc1c9e88bc9893
Ole Magnus Fon Johnsen <me@omfj.no>
2023-12-19 17:33:54 +0100

    Dark mode form (#1053)
    Co-authored-by: Zeno Elio Leonardi <zeleo1679@uib.no>

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary-hover",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border border-input hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-border hover:bg-accent hover:text-accent-foreground",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "underline-offset-4 hover:underline text-primary",
