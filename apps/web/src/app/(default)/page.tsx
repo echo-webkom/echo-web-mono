a7de50d0584e0ee0a22890da3e31a74d820e38e8
Ole Magnus Fon Johnsen <me@omfj.no>
2024-01-03 16:25:10 +0100

    Use getAuth
    Jeg tror dette kan gjøre at hovedsiden laster inn raskere, siden den bruker samme request som den i layout.tsx på header.

--- apps/web/src/app/(default)/page.tsx
+++ apps/web/src/app/(default)/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-import { getAuthSession } from "@echo-webkom/auth";
+import { getAuth } from "@echo-webkom/auth";
 
 import { Container } from "@/components/container";
 import { Button } from "@/components/ui/button";
 import { Content } from "./content";
 
 export default async function HomePage() {
-  const session = await getAuthSession();
+  const session = await getAuth();
 
   return (
     <>
