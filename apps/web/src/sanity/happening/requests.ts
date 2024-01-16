8afcd147e0298f73563f4eea9af0127f46ab8c9b
elefantoft <KjetilAlvestad@gmail.com>
2024-01-16 19:21:21 +0100

    add deprecated

--- apps/web/src/sanity/happening/requests.ts
+++ apps/web/src/sanity/happening/requests.ts
@@ -10,6 +10,7 @@ import { happeningSchema, type Happening, type HappeningType } from "./schemas";
  * Fetches all happenings
  *
  * @returns all happenings
+ * @deprecated DO NOT USE
  */
 export async function fetchAllHappenings() {
   return await sanityFetch<Array<Happening>>({
