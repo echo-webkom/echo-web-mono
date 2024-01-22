e5c6ad7cb84f872820694a7f9f0103b85fddd4bd
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-01-22 10:16:39 +0100

    Litt styling

--- apps/web/src/components/hyggkom-shopping-list.tsx
+++ apps/web/src/components/hyggkom-shopping-list.tsx
@@ -59,13 +59,16 @@ export function HyggkomShoppingList({ items }: hyggkomShoppingListProps) {
   return (
     <div>
       <h3>Like de tingene du mener vi bør kjøpe inn, eller legg til ditt eget forslag!</h3>
-      <ul className="list-group capitalize border rounded-lg">
-        {items.map((i) => {
+      <ul className="list-group capitalize">
+        <li className="grid grid-cols-5 py-2 px-2 border-b-black bg-slate-300">
+          <p className="col-span-4 font-bold">Forslag</p>
+          <p className="font-bold">Like</p>
+        </li>
+        {items.map((i, index) => {
           return (
-          <li className="grid grid-cols-5 py-2 px-2" key={i.item.id}>
-            <p className="col-span-3">{i.item.name}</p>
-            <p className="col-span-1">{i.item.likesCount}</p>
-            <button className={`col-span-1 border rounded-xl ${i.isLiked? "bg-red-400" : "bg-white"}`} onClick={() => handleButtonClick(i.item.id)}>like</button>
+          <li className={`grid grid-cols-5 py-2 px-2 ${index%2 === 0? "bg-white" : "bg-slate-100"} border`}  key={i.item.id}>
+            <p className="col-span-4">{i.item.name}</p>
+            <button className={`col-span-1 border rounded-xl ${i.isLiked? "bg-red-400" : "bg-white"} hover:bg-red-200`} onClick={() => handleButtonClick(i.item.id)}>{i.item.likesCount}</button>
           </li>)
         })}
       </ul>
