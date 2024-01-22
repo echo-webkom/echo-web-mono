accec7eb706c2f20d2593930b096659692c875d2
Ole Magnus Fon Johnsen <me@omfj.no>
2024-01-22 23:13:31 +0100

    fix domain name

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -2,7 +2,7 @@ export const PRODUCTION_DOMAIN = "echo.uib.no";
 
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? PRODUCTION_DOMAIN
+    ? `https://${PRODUCTION_DOMAIN}`
     : `http://localhost:${process.env.PORT ?? 3000}`;
 
 export const COOKIE_BANNER = "cookie-banner";
