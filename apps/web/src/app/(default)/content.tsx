c275672d5daaba1962b4265731c69ec89c0305f6
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-01-30 11:45:18 +0100

    Ferdigstillt layout

--- apps/web/src/app/(default)/content.tsx
+++ apps/web/src/app/(default)/content.tsx
@@ -32,7 +32,7 @@ export async function Content() {
   const [events, bedpresses, posts, jobAds] = await Promise.all([
     fetchHomeHappenings(["event", "external"], 4),
     fetchHomeHappenings(["bedpres"], 4),
-    fetchPosts(4),
+    fetchPosts(2),
     fetchAvailableJobAds(4),
   ]);
 
@@ -99,26 +99,6 @@ export async function Content() {
         </ul>
       </section>
 
-      {/* Posts */}
-      <section className="flex flex-col gap-5 rounded-md border p-5 shadow-lg lg:col-span-2">
-        <Link href="/for-studenter/innlegg">
-          <h2 className="group text-center text-xl font-semibold decoration-1 underline-offset-8 hover:underline md:text-3xl">
-            Siste nytt
-            <ArrowRight className="ml-2 inline h-4 w-4 transition-transform group-hover:translate-x-2" />
-          </h2>
-        </Link>
-
-        <hr />
-
-        <ul className="grid grid-cols-1 gap-x-3 gap-y-5 py-4 lg:grid-cols-2">
-          {posts.map((post) => (
-            <li key={post._id}>
-              <PostPreview post={post} className="shadow-none" />
-            </li>
-          ))}
-        </ul>
-      </section>
-
       {/* Job ads */}
       {jobAds.length > 0 && (
         <section className="flex flex-col gap-5 rounded-md border p-5 shadow-lg lg:col-span-2">
@@ -137,6 +117,26 @@ export async function Content() {
           </ul>
         </section>
       )}
+
+      {/* Posts */}
+      <section className="flex flex-col gap-5 rounded-md border p-5 shadow-lg lg:col-span-1">
+        <Link href="/for-studenter/innlegg">
+          <h2 className="group text-center text-xl font-semibold decoration-1 underline-offset-8 hover:underline md:text-3xl">
+            Siste nytt
+            <ArrowRight className="ml-2 inline h-4 w-4 transition-transform group-hover:translate-x-2" />
+          </h2>
+        </Link>
+
+        <hr />
+
+        <ul className="grid grid-cols-1 gap-x-3 gap-y-5 py-4 lg:grid-cols-2">
+          {posts.map((post) => (
+            <li key={post._id}>
+              <PostPreview post={post} className="shadow-none" />
+            </li>
+          ))}
+        </ul>
+      </section>
 
       {/* Hyggkom handleliste */}
       <section className="flex flex-col gap-5 rounded-md border p-5 shadow-lg lg:col-span-1">
