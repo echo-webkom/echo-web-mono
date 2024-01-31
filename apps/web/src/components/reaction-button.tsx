fe2a79eac0aa5a4d223d8d5293ccd7fbe5f1a3de
Johanne Blikberg Herheim <joher2128@uib.no>
2024-01-31 18:20:06 +0100

    fix lint error

--- apps/web/src/components/reaction-button.tsx
+++ apps/web/src/components/reaction-button.tsx
@@ -19,9 +19,8 @@ export function ReactionButtons({ reactions, reactToKey }: ReactionButtonProps) 
       {Object.entries(idToEmoji).map(([key, value]) => {
         const reactToPage = handleReact.bind(null, reactToKey, parseInt(key));
         return (
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
           <form key={key} action={reactToPage}>
-            <input type="hidden" name="emojiId" value={key} />
-            <input type="hidden" name="react_to_key" value={reactToKey} />
             <Button
               type="submit"
               className={`${reactions[Number(key)]?.hasReacted ? "bg-wave hover:bg-wave" : "bg-muted hover:bg-muted"} h-8 w-14 rounded-full text-foreground`}
