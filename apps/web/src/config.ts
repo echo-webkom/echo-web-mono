e3da0dfe54455c03c30bd623e3d1ab90e99e0bb1
Torger Bocianowski <84389241+bocianowski1@users.noreply.github.com>
2024-01-31 13:40:04 +0100

    Profile pictures (#1276)

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -5,4 +5,6 @@ export const baseURL =
     ? `https://${PRODUCTION_DOMAIN}`
     : `http://localhost:${process.env.PORT ?? 3000}`;
 
+export const PROFILE_IMAGE_FUNCTION_URL = "https://echo-images.azurewebsites.net/api/images";
+
 export const COOKIE_BANNER = "cookie-banner";
