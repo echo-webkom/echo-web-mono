11c7e8b4ac3de962a143c5a323f5a92bd2f41947
Ole Magnus Fon Johnsen <me@omfj.no>
2024-02-01 10:41:34 +0100

    Remove docs from mono (#1347)

--- playwright/docs/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("docs frontpage", async ({ page }) => {
-  await page.goto("http://localhost:3001/");
-
-  await expect(page).toHaveTitle("Index - echo Webkom Docs");
-});
