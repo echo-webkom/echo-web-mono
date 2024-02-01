80c2e63831a84b14674caeb55cf78d086de1dfc3
Ole Magnus Fon Johnsen <me@omfj.no>
2024-02-01 11:39:02 +0100

    Revalidate static content

--- apps/web/src/app/api/sanity/revalidate/route.ts
+++ apps/web/src/app/api/sanity/revalidate/route.ts
@@ -2,36 +2,66 @@ import { revalidateTag } from "next/cache";
 
 import { withBasicAuth } from "@/lib/checks/with-basic-auth";
 
-const typeToRevalidate = {
-  post: ["posts"],
-  company: ["jobAds"],
-  job: ["jobAds"],
-  profile: [""],
-  staticInfo: ["staticInfo"],
-};
-
 const revalidateTags = (tags: Array<string>) => {
   for (const tag of tags) {
     revalidateTag(tag);
   }
 };
 
-const isValidType = (type: string): type is keyof typeof typeToRevalidate =>
-  Object.keys(typeToRevalidate).includes(type);
-
+/**
+ * Updates static content on the site.
+ *
+ * Data is sent from a GROQ projection in Sanity:
+ * ```
+ * {
+ *   "operation": delta::operation(),
+ *   "documentId": _id,
+ *   "type": _type,
+ *   "slug": slug.current,
+ * }
+ * ```
+ */
 export const POST = withBasicAuth(async (req) => {
   try {
-    const { type } = (await req.json()) as { type: string };
+    const { type, slug } = (await req.json()) as {
+      operation: "create" | "update" | "delete";
+      documentId: string;
+      type: string;
+      slug: string | null;
+    };
 
-    if (isValidType(type)) {
-      revalidateTags(typeToRevalidate[type]);
-      return new Response(`Revalidated ${type}`, { status: 200 });
+    if (type === "staticInfo") {
+      revalidateTags(["static-info"]);
     }
 
-    return new Response(`We have not implemented ${type} yet.`, {
-      status: 400,
+    if (type === "jobAds") {
+      revalidateTags(["jobAds"]);
+    }
+
+    if (type === "posts") {
+      revalidateTags(["posts"]);
+    }
+
+    if (type === "meetingMinute") {
+      revalidateTags(["minutes"]);
+    }
+
+    if (type === "studentGroup") {
+      revalidateTags(["student-groups"]);
+      if (slug) {
+        revalidateTags([`student-group-${slug}`]);
+      }
+    }
+
+    return new Response(`Revalidated type: "${type}".`, {
+      status: 200,
     });
-  } catch {
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      console.error(error);
+      return new Response("Invalid request", { status: 400 });
+    }
+
     return new Response("Invalid request", { status: 400 });
   }
 });
