c01c35238616685170c66ead7bc96c57c2110685
Johanne Blikberg Herheim <joher2128@uib.no>
2024-02-04 21:16:52 +0100

    optimistic count works ish

--- apps/web/src/components/reaction-button.tsx
+++ apps/web/src/components/reaction-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useOptimistic } from "react";
+import { useOptimistic, useState } from "react";
 
 import { cn } from "@/utils/cn";
 import { Button } from "./ui/button";
@@ -12,24 +12,31 @@ type ReactionButtonProps = {
 };
 
 export default function ReactionButton({ value, hasReacted, count }: ReactionButtonProps) {
-  const [optimisticCount, addOptimisticCount] = useOptimistic(count, (currentCount, newCount) => {
-    return currentCount + 2;
+  const [isClicked, setIsClicked] = useState(hasReacted ? true : false);
+
+  const setBacgroundColor = (state: boolean) => {
+    return state
+      ? "bg-reaction hover:bg-reaction text-foreground"
+      : "sm:hover:bg-reaction hover:bg-muted bg-muted text-foreground";
+  };
+
+  const handleButtonClick = () => {
+    setIsClicked(!isClicked);
+    isClicked ? addOptimisticCount(1) : addOptimisticCount(-1);
+  };
+
+  const [optimisticCount, addOptimisticCount] = useOptimistic(count, (currentCount) => {
+    return isClicked ? currentCount + 1 : currentCount - 1;
   });
 
   return (
-    <Button
-      type="submit"
-      className={cn(
-        "h-8 w-14 rounded-full",
-        hasReacted
-          ? "bg-reaction hover:bg-reaction text-foreground"
-          : "hover:bg-reaction bg-muted text-foreground",
-      )}
-    >
-      <div className="flex gap-1 font-normal">
-        <p>{value}</p>
-        <p>{optimisticCount ?? 0}</p>
-      </div>
-    </Button>
+    <div onClick={handleButtonClick}>
+      <Button type="submit" className={cn("h-8 w-14 rounded-full", setBacgroundColor(isClicked))}>
+        <div className="flex gap-1 font-normal">
+          <p>{value}</p>
+          <p>{optimisticCount ?? 0}</p>
+        </div>
+      </Button>
+    </div>
   );
 }
