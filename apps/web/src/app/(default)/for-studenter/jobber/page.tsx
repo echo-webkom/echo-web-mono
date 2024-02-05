511341b9520d216c705a4a7555b0db87c0e5ea05
Zeno Elio Leonardi <zeleo1679@uib.no>
2024-02-05 17:30:49 +0100

    Initial commit

--- apps/web/src/app/(default)/for-studenter/jobber/page.tsx
+++ apps/web/src/app/(default)/for-studenter/jobber/page.tsx
@@ -16,7 +16,7 @@ export default async function JobAdsOverviewPage() {
     <Container>
       <Heading>Stillingsannonser</Heading>
 
-      <ul className="grid grid-cols-1 lg:grid-cols-2">
+      <ul className="pt-4 space-y-4 max-w-2xl">
         {jobAds.map((jobAd) => (
           <li key={jobAd._id}>
             <JobAdPreview jobAd={jobAd} />
