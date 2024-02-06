8461a4aacc3485d65218e78f0babcfa6ca7de184
Zeno Elio Leonardi <zeleo1679@uib.no>
2024-02-06 14:34:30 +0100

    La til job-ad-filter

--- apps/web/src/app/(default)/for-studenter/jobber/page.tsx
+++ apps/web/src/app/(default)/for-studenter/jobber/page.tsx
@@ -1,6 +1,11 @@
 import { type Metadata } from "next/types";
 
 import { Container } from "@/components/container";
+import {
+  FilterStatusAndOrderBar,
+  JobAdFilter,
+  JobAdFilterSidebar,
+} from "@/components/job-ad-filter";
 import { JobAdPreview } from "@/components/job-ad-preview";
 import { Heading } from "@/components/typography/heading";
 import { fetchJobAds } from "@/sanity/job-ad";
@@ -15,8 +20,11 @@ export default async function JobAdsOverviewPage() {
   return (
     <Container>
       <Heading>Stillingsannonser</Heading>
+      <JobAdFilter />
+      <JobAdFilterSidebar />
+      <FilterStatusAndOrderBar />
 
-      <ul className="pt-4 space-y-4 max-w-2xl">
+      <ul className="max-w-2xl space-y-4 pt-4">
         {jobAds.map((jobAd) => (
           <li key={jobAd._id}>
             <JobAdPreview jobAd={jobAd} />
