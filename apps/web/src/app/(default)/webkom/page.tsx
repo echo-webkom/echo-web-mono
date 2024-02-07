bdd08d01fdea29514a96a8e573e587e91fef5a15
Ole Magnus Fon Johnsen <me@omfj.no>
2024-02-07 02:01:58 +0100

    Fix webkom page

--- apps/web/src/app/(default)/webkom/page.tsx
+++ apps/web/src/app/(default)/webkom/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import Webkom from "@/assets/images/webkom.png";
 import { Container } from "@/components/container";
 import { Heading } from "@/components/typography/heading";
 import { UnorderedList } from "@/components/typography/list";
@@ -9,19 +10,17 @@ import { Text } from "@/components/typography/text";
 export default function WebkomPage() {
   return (
     <Container className="text-center">
-      <Image
-        className="mx-auto"
-        src="/images/webkom.png"
-        width={200}
-        height={200}
-        alt="Webkom logo"
-      />
+      <Image className="mx-auto h-32 w-auto" src={Webkom} alt="Webkom logo" />
       <Heading className="font-display">Webkom</Heading>
 
-      <Text className="text-gray-800">Tidligere og nåværende medlemmer av echo Webkom.</Text>
+      <Text className="text-muted-foreground">
+        Tidligere og nåværende medlemmer av echo Webkom.
+      </Text>
+
+      <Text className="text-sm italic text-muted-foreground">EST. 2020</Text>
 
       <div className="mx-auto my-10 max-w-md">
-        <UnorderedList className="list-none text-lg">
+        <UnorderedList className="list-none p-0 text-lg">
           <ListItem>Andreas Bakseter (Co-founder)</ListItem>
           <ListItem>Bo Aanes (Co-founder)</ListItem>
 
@@ -31,20 +30,23 @@ export default function WebkomPage() {
           <ListItem>Øyvind Grutle</ListItem>
           <ListItem>Sander Sigmundstad</ListItem>
           <ListItem>Victoria Valner</ListItem>
+
+          {/* --- H21 --- */}
           <ListItem>Nikolaus Engh</ListItem>
-          {/* --- */}
           <ListItem>Ole Magnus Fon Johnsen</ListItem>
           <ListItem>Mathilde Bergenheim</ListItem>
           <ListItem>Thea Jenny Kolnes</ListItem>
           <ListItem>Felix Kaasa</ListItem>
-          {/* --- */}
+
+          {/* --- H22 --- */}
           <ListItem>Torger Bocianowski</ListItem>
           <ListItem>Malin Torset Sivertstøl</ListItem>
           <ListItem>Karolina Gil</ListItem>
           <ListItem>Leoul Zinaye Tefera</ListItem>
           <ListItem>Kjetil Alvestad</ListItem>
           <ListItem>Jonas Hammerseth</ListItem>
-          {/* --- */}
+
+          {/* --- H23 --- */}
           <ListItem>Johanne Blikberg Herheim</ListItem>
           <ListItem>Kristian Elde Johansen</ListItem>
           <ListItem>Swarnika Sellathurai</ListItem>
