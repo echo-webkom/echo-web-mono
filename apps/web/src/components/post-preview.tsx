860f14009afa02c5ba8735dec17ef65e0a94881b
Kjetil Fantoft Alvestad <KjetilAlvestad@gmail.com>
2024-02-08 12:06:44 +0100

    Make new posts POP (#1394)

--- apps/web/src/components/post-preview.tsx
+++ apps/web/src/components/post-preview.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { format } from "date-fns";
+import { addDays, format } from "date-fns";
 import { nb } from "date-fns/locale/nb";
 import removeMd from "remove-markdown";
 
@@ -15,15 +15,19 @@ type PostPreviewProps = {
 };
 
 export function PostPreview({ post, withBorder = false, className }: PostPreviewProps) {
+  const isNew = addDays(new Date(post._createdAt), 3) > new Date();
+
   return (
     <Link href={`/for-studenter/innlegg/${post.slug}`}>
       <div
         className={cn(
           "relative flex h-full flex-col gap-1 rounded-lg p-5 shadow-lg transition-colors duration-200 ease-in-out hover:bg-muted",
           withBorder && "border",
+          isNew && "bg-muted hover:bg-transparent",
           className,
         )}
       >
+        {isNew && <Chip className="absolute -top-2 left-0 bg-primary text-white">NY</Chip>}
         <h3 className="line-clamp-2 flex gap-2 text-xl font-semibold md:text-2xl">{post.title}</h3>
 
         <p className="right-1 top-1 text-sm text-gray-500 sm:absolute">
@@ -35,7 +39,7 @@ export function PostPreview({ post, withBorder = false, className }: PostPreview
         <p className="my-2 line-clamp-2 italic">{removeMd(post.body)}</p>
 
         {post.authors && (
-          <div className="flex flex-row flex-wrap items-center gap-1 sm:absolute sm:-bottom-3 sm:left-4">
+          <div className="flex flex-row flex-wrap items-center gap-1 sm:absolute sm:-bottom-1 sm:right-4">
             {post.authors.map((author) => (
               <Chip className="bg-secondary text-secondary-foreground" key={author._id}>
                 {isBoard(author.name) ? "Hovedstyret" : author.name}
