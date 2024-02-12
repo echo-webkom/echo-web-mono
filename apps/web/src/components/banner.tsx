f363c56af3453357c42ea525c418913fe2eb7261
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-02-12 09:28:24 +0100

    tydeligere tekst i banneret

--- apps/web/src/components/banner.tsx
+++ apps/web/src/components/banner.tsx
@@ -9,7 +9,7 @@ export default function VervBanner() {
         className="flex h-full w-full items-center justify-center space-x-3 rounded-md bg-primary bg-gradient-to-r from-banner to-primary py-4 text-base font-semibold text-white sm:space-x-5 sm:text-2xl"
         suppressHydrationWarning
       >
-        <span>Søk verv i Webkom eller Gnist! Søknadsfrist 14. Februar</span>
+        <span>Søk verv i Webkom eller Gnist her! Søknadsfristen er 14. Februar</span>
       </div>
     </Link>
   );
