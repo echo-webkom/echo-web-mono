9396b39b63a85e76c4bf82eb16a480333dd1b02f
Izaak Sarnecki <izaaksarnecki@outlook.com>
2024-02-12 12:45:19 +0100

    change query to only query valid notifs

--- apps/web/src/sanity/notifications/queries.ts
+++ apps/web/src/sanity/notifications/queries.ts
@@ -1,7 +1,7 @@
 import { groq } from "next-sanity";
 
 export const allNotificationsQuery = groq`
-*[_type == "notification" && !(_id in path('drafts.**'))] | order(_createdAt desc) {
+*[_type == "notification" && !(_id in path('drafts.**')) && validTo > now()] | order(_createdAt desc) {
   _id,
   title,
   subtitle,
