ee38da2399e3b2c4a195561a1fb2db9d04cf5e56
Zeno Elio Leonardi <145581496+0Zeno@users.noreply.github.com>
2024-02-14 12:08:32 +0100

    Fikset underline på sistenytt kortet (#1433)

--- apps/web/src/app/(default)/content.tsx
+++ apps/web/src/app/(default)/content.tsx
@@ -118,8 +118,11 @@ export async function Content() {
 
       {/* Posts */}
       <section className="flex flex-col gap-5 rounded-md border p-5 shadow-lg lg:col-span-1">
-        <Link href="/for-studenter/innlegg">
-          <h2 className="group text-center text-xl font-semibold decoration-1 underline-offset-8 hover:underline md:text-3xl">
+        <Link
+          href="/for-studenter/innlegg"
+          className="group mx-auto flex items-center underline-offset-4 hover:underline"
+        >
+          <h2 className="group text-center text-xl font-semibold decoration-1 md:text-3xl">
             Siste nytt
             <ArrowRight className="ml-2 inline h-6 w-6 transition-transform group-hover:translate-x-2" />
           </h2>
