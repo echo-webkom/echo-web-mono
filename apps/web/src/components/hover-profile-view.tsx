6c551419aaacf69800c5036dd7a7c5c28136b1f1
Johanne Blikberg Herheim <145448180+johanneherheim@users.noreply.github.com>
2024-03-01 08:20:55 +0100

    Fix popover (#1514)

--- apps/web/src/components/hover-profile-view.tsx
+++ apps/web/src/components/hover-profile-view.tsx
@@ -1,13 +1,11 @@
-import { useRef, useState } from "react";
 import Link from "next/link";
 import { LuClock, LuGraduationCap, LuMail, LuUsers } from "react-icons/lu";
 import { RxInfoCircled } from "react-icons/rx";
 
 import { type User } from "@echo-webkom/db/schemas";
 
-import { useOutsideClick } from "@/hooks/use-outsideclick";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { mailTo } from "@/utils/prefixes";
-import { Button } from "./ui/button";
 
 export function HoverProfileView({
   user,
@@ -18,51 +16,38 @@ export function HoverProfileView({
   group: string;
   changedAt: string | null;
 }) {
-  const [isClicked, setIsClicked] = useState(false);
-  const profileRef = useRef(null);
-
-  useOutsideClick(() => setIsClicked(false), [profileRef]);
-
   return (
-    <div className="flex" ref={profileRef}>
-      <Button
-        className="p-0 hover:bg-transparent"
-        variant="ghost"
-        onClick={() => setIsClicked(!isClicked)}
-      >
-        <RxInfoCircled className="size-5 origin-center transition-all hover:size-6" />
-      </Button>
-      {isClicked && (
-        <div className="absolute left-16 flex flex-row gap-4 rounded-lg border bg-background p-4 text-foreground sm:left-24 ">
-          <div className="flex flex-col gap-1">
-            <p className="font-bold">{user.name}</p>
-            {user.year && user.degreeId && (
-              <p className="flex text-muted-foreground">
-                <LuGraduationCap className="my-auto mr-2" />
-                {user.year.toString() + ". trinn " + user.degreeId.toUpperCase()}
-              </p>
-            )}
-            {group.length > 0 && (
-              <p className="flex text-muted-foreground">
-                <LuUsers className="my-auto mr-2" />
-                {group}
-              </p>
-            )}
-            <p className="flex text-muted-foreground">
-              <LuMail className="my-auto mr-2" />
-              <Link className="hover:underline" href={mailTo(user.email)}>
-                {user.email}
-              </Link>
-            </p>
-            {changedAt && (
-              <p className="flex text-muted-foreground">
-                <LuClock className="my-auto mr-2" />
-                {changedAt}
-              </p>
-            )}
-          </div>
-        </div>
-      )}
-    </div>
+    <Popover>
+      <PopoverTrigger className="my-auto p-1">
+        <RxInfoCircled className="size-5" />
+      </PopoverTrigger>
+      <PopoverContent className="mx-2 flex w-fit flex-col gap-1">
+        <p className="font-bold">{user.name}</p>
+        {user.year && user.degreeId && (
+          <p className="flex text-muted-foreground">
+            <LuGraduationCap className="my-auto mr-2" />
+            {user.year.toString() + ". trinn " + user.degreeId.toUpperCase()}
+          </p>
+        )}
+        {group.length > 0 && (
+          <p className="flex text-muted-foreground">
+            <LuUsers className="my-auto mr-2" />
+            {group}
+          </p>
+        )}
+        <p className="flex text-muted-foreground">
+          <LuMail className="my-auto mr-2" />
+          <Link className="hover:underline" href={mailTo(user.email)}>
+            {user.alternativeEmail ?? user.email}
+          </Link>
+        </p>
+        {changedAt && (
+          <p className="flex text-muted-foreground">
+            <LuClock className="my-auto mr-2" />
+            {changedAt}
+          </p>
+        )}
+      </PopoverContent>
+    </Popover>
   );
 }
