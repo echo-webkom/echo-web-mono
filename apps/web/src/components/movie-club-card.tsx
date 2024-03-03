bc2b329f51af25748120cb21f787b19ebcb1727c
Zeno Elio Leonardi <zeleo1679@uib.no>
2024-03-03 19:23:00 +0100

    Filmkort på hjemmesiden og ny sanity til fimklubben

--- apps/web/src/components/movie-club-card.tsx
+++ apps/web/src/components/movie-club-card.tsx
@@ -1,21 +1,42 @@
 import Image from "next/image";
-import { movies } from "@/sanity/movies";
 
-export default function MovieClubCard() {
+import { fetchMovies } from "@/sanity/movies";
+import { shortDate } from "@/utils/date";
+import { urlFor } from "@/utils/image-builder";
+
+export default async function MovieClubCard() {
+  const movies = await fetchMovies();
+
+  const thisWeekMovie = movies[0];
+
+  if (!thisWeekMovie) {
+    return null;
+  }
   return (
-    <div>
-      <Image src={movies.image} alt={movies.title}></Image>
-      <ul>
-        <li>
-          <span className="font-semibold">Tittel:</span> {movies.title}
-        </li>
-        <li>
-          <span className="font-semibold">Sted:</span> Store Aud
-        </li>
-        <li>
-          <span className="font-semibold">Dato:</span> {movies.datetime}
-        </li>
-      </ul>
+    <div className="flex justify-center p-5">
+      <div className="relative h-96 w-64">
+        <div>
+          <Image
+            className="rounded-md border"
+            fill
+            src={urlFor(thisWeekMovie?.image).url()}
+            alt={`${thisWeekMovie.title} logo`}
+          />
+          <div className="absolute inset-0 flex flex-col-reverse rounded-md border p-2">
+            <div className="rounded-md bg-background p-2 opacity-90">
+              <h1 className="font-semibold">{thisWeekMovie?.title}</h1>
+              <ul className="content-evenly text-sm">
+                <li>
+                  <span className="font-semibold">Sted:</span> Store Aud
+                </li>
+                <li>
+                  <span className="font-semibold">Dato:</span> {shortDate(thisWeekMovie?.date)}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- apps/web/src/sanity/movies/schemas.ts
+++ apps/web/src/sanity/movies/schemas.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
+
 import { imageSchema } from "../utils/image";
 
 export const moviesSchema = z.object({
+  title: z.string(),
+  date: z.string(),
+  link: z.string(),
+  image: imageSchema,
+});
 
-    title: z.string(),
-    date: z.string(),
-    link: z.string(),
-    Image: imageSchema,
-
-})
+export type Movies = z.infer<typeof moviesSchema>;
