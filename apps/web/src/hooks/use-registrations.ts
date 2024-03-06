1fdac59662689c377697fdabf402d3cd522918a7
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-06 18:30:23 +0100

    Use wss (#1547)

--- apps/web/src/hooks/use-registrations.ts
+++ apps/web/src/hooks/use-registrations.ts
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 
-const BASE_WS_URL = `ws://${process.env.NEXT_PUBLIC_BOOMTOWN_HOSTNAME}`;
+const BASE_WS_URL = `${process.env.NODE_ENV === "production" ? "wss" : "ws"}://${process.env.NEXT_PUBLIC_BOOMTOWN_HOSTNAME}`;
 
 export function useRegistrations(
   happeningId: string,
