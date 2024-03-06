cb4e3d804d51d183d570d8d37ba217f22b6a215d
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-06 18:11:27 +0100

    Implement boomtown

---
+++ apps/web/src/api/boomtown.ts
@@ -0,0 +1,10 @@
+export const BOOMTOWN_HOSTNAME = process.env.NEXT_PUBLIC_BOOMTOWN_HOSTNAME;
+
+export async function pingBoomtown(happeningId: string) {
+  return await fetch(
+    `${process.env.NODE_ENV === "production" ? "https" : "http"}://${BOOMTOWN_HOSTNAME}/${happeningId}`,
+    {
+      method: "POST",
+    },
+  ).then((response) => response.status === 200);
+}
---
+++ apps/web/src/hooks/use-registrations.ts
@@ -0,0 +1,41 @@
+import { useEffect, useMemo, useState } from "react";
+
+const BASE_WS_URL = `ws://${process.env.NEXT_PUBLIC_BOOMTOWN_HOSTNAME}`;
+
+export function useRegistrations(
+  happeningId: string,
+  initialRegistrationCount: number,
+  initialWaitlistCount: number,
+) {
+  const [registeredCount, setRegisteredCount] = useState(initialRegistrationCount);
+  const [waitlistCount, setWaitlistCount] = useState(initialWaitlistCount);
+
+  const ws = useMemo(() => new WebSocket(`${BASE_WS_URL}/ws/${happeningId}`), [happeningId]);
+
+  useEffect(() => {
+    ws.onmessage = (event) => {
+      const message = JSON.parse(event.data as string) as {
+        registerCount: number;
+        waitlistCount: number;
+      };
+      setRegisteredCount(message.registerCount);
+      setWaitlistCount(message.waitlistCount);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [ws]);
+
+  if (!BASE_WS_URL) {
+    return {
+      registeredCount: initialRegistrationCount,
+      waitlistCount: initialWaitlistCount,
+    };
+  }
+
+  return {
+    registeredCount,
+    waitlistCount,
+  };
+}
