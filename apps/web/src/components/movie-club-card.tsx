a275782a4c3ade742ef279ff33035c9593b00549
Zeno Elio Leonardi <zeleo1679@uib.no>
2024-03-07 15:11:31 +0100

    La til responsivt design og bedre design

--- apps/web/src/components/movie-club-card.tsx
+++ apps/web/src/components/movie-club-card.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 import { isPast } from "date-fns/isPast";
 
 import { fetchNewestMovie } from "@/sanity/movies";
-import { shortDate } from "@/utils/date";
+import { shortDateNoTimeNoYear, shortDateNoYear } from "@/utils/date";
 import { urlFor } from "@/utils/image-builder";
 
 export default async function MovieClubCard() {
-  const movies = await fetchNewestMovie(1);
+  const movies = await fetchNewestMovie(3);
 
   const thisWeekMovie = movies[0];
+  const nextWeekMovie = movies[1];
+  const nextNextWeekMovie = movies[2];
 
   if (!thisWeekMovie) {
     return (
@@ -25,32 +27,71 @@ export default async function MovieClubCard() {
     );
   }
   return (
-    <div className="flex justify-center p-5">
-      <div className="relative h-96 w-64">
-        <div>
-          <Link href={thisWeekMovie.link}>
+    <div className="flex justify-evenly">
+      <div className="flex flex-col">
+        <div className="group relative h-[20rem] w-[13rem] sm:h-[24rem] sm:w-[16rem]">
+          <div>
             <Image
               className="rounded-md border"
               fill
-              src={urlFor(thisWeekMovie?.image).url()}
+              src={urlFor(thisWeekMovie.image).url()}
               alt={`${thisWeekMovie.title} logo`}
             />
-          </Link>
-          <div className="absolute inset-0 flex flex-col-reverse rounded-md border p-2">
-            <div className="rounded-md bg-background p-2 opacity-90">
-              <h1 className="font-semibold">{thisWeekMovie?.title}</h1>
-              <ul className="content-evenly text-sm">
-                <li>
-                  <span className="font-semibold">Sted:</span> Store Aud
-                </li>
-                <li>
-                  <span className="font-semibold">Dato:</span> {shortDate(thisWeekMovie?.date)}
-                </li>
-              </ul>
-            </div>
           </div>
+          <Link href={thisWeekMovie.link}>
+            <div className="absolute inset-0 flex flex-col-reverse overflow-hidden rounded-md border p-2">
+              <div className="rounded-md bg-background p-2 opacity-90 transition-transform duration-300 group-hover:translate-y-40">
+                <ul>
+                  <label className="line-clamp-1 overflow-hidden text-2xl font-semibold">
+                    {thisWeekMovie.title}
+                  </label>
+                  <li>
+                    <span className="font-semibold">Sted:</span> Store Aud
+                  </li>
+                  <li>
+                    <span className="font-semibold">Dato:</span>{" "}
+                    {shortDateNoYear(thisWeekMovie.date)}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
+      {nextWeekMovie && (
+        <div>
+          <ul className="flex flex-col space-y-8">
+            <div className="relative h-[9rem] w-[6rem] sm:h-[11rem] sm:w-[7rem]">
+              <Image
+                className="rounded-md border"
+                fill
+                src={urlFor(nextWeekMovie.image).url()}
+                alt={`${nextWeekMovie.title} logo`}
+              />
+              <div className="absolute inset-0 flex flex-col-reverse overflow-hidden rounded-md border p-2">
+                <li className="rounded-md bg-background p-2 text-xs opacity-90">
+                  {shortDateNoTimeNoYear(nextWeekMovie.date)}
+                </li>
+              </div>
+            </div>
+            {nextNextWeekMovie && (
+              <div className="relative h-[9rem] w-[6rem] sm:h-[11rem] sm:w-[7rem]">
+                <Image
+                  className="rounded-md border"
+                  fill
+                  src={urlFor(nextNextWeekMovie.image).url()}
+                  alt={`${nextNextWeekMovie.title} logo`}
+                />
+                <div className="absolute inset-0 flex flex-col-reverse overflow-hidden rounded-md border p-2">
+                  <li className="rounded-md bg-background p-2 text-xs opacity-90">
+                    {shortDateNoTimeNoYear(nextNextWeekMovie.date)}
+                  </li>
+                </div>
+              </div>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
