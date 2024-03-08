5971b44f538d004d1b397d230d485e8c0bb9d5e0
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-09 00:06:07 +0100

    split future and past events

--- apps/web/src/app/(default)/auth/profil/arrangementer/page.tsx
+++ apps/web/src/app/(default)/auth/profil/arrangementer/page.tsx
@@ -11,6 +11,7 @@ import {
 import { Chip } from "@/components/typography/chip";
 import { Heading } from "@/components/typography/heading";
 import { getRegistrationsByUserId } from "@/data/registrations/queries";
+import { shortDateNoTime } from "@/utils/date";
 
 export default async function UserHappenings() {
   const user = await auth();
@@ -20,35 +21,87 @@ export default async function UserHappenings() {
   }
   const registrations = await getRegistrationsByUserId(user.id);
 
+  const pastRegistrations = registrations
+    .slice()
+    .reverse()
+    .filter(
+      (registration) =>
+        !registration.happening.date || new Date(registration.happening.date) < new Date(),
+    );
+  const futureRegistrations = registrations
+    .slice()
+    .reverse()
+    .filter(
+      (registration) =>
+        registration.happening.date && new Date(registration.happening.date) >= new Date(),
+    );
+
   return (
     <div className="max-w-2xl">
       <Heading level={2} className="mb-4">
         Dine arrangementer
       </Heading>
       {registrations.length > 0 ? (
-        <ul className="flex flex-col divide-y">
-          {registrations.map((registration) => (
-            <li key={registration.happening.slug}>
-              <div className="py-3">
-                <Link
-                  href={
-                    happeningTypeToPath[registration.happening.type] +
-                    "/" +
-                    registration.happening.slug
-                  }
-                  className="text-lg font-semibold hover:underline"
-                >
+        <div className="w-fit">
+          <Heading level={3} className="mt-14 pb-5 font-semibold">
+            Kommende arrangementer
+          </Heading>
+          <div className="flex flex-col">
+            {futureRegistrations.map((registration) => (
+              <Link
+                href={
+                  happeningTypeToPath[registration.happening.type] +
+                  "/" +
+                  registration.happening.slug
+                }
+                key={registration.happening.slug}
+                className="rounded-md p-4 hover:bg-muted"
+              >
+                <h1 className="my-auto line-clamp-1 overflow-hidden text-lg sm:text-2xl">
                   {registration.happening.title}
-                </Link>
-
+                </h1>
+                {registration.happening.date && (
+                  <p className="text-sm text-muted-foreground">
+                    {shortDateNoTime(registration.happening.date)}
+                  </p>
+                )}
                 <div className="mt-3 flex gap-1">
                   <Chip>{happeningTypeToString[registration.happening.type]}</Chip>
                   <Chip>{registrationStatusToString[registration.status]}</Chip>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </Link>
+            ))}
+          </div>
+          <Heading level={3} className="mt-14 pb-5 font-semibold">
+            Tidligere arrangementer
+          </Heading>
+          <div className="flex flex-col">
+            {pastRegistrations.map((registration) => (
+              <Link
+                href={
+                  happeningTypeToPath[registration.happening.type] +
+                  "/" +
+                  registration.happening.slug
+                }
+                key={registration.happening.slug}
+                className="rounded-md p-4 hover:bg-muted"
+              >
+                <h1 className="my-auto line-clamp-1 overflow-hidden text-lg sm:text-2xl">
+                  {registration.happening.title}
+                </h1>
+                {registration.happening.date && (
+                  <p className="text-sm text-muted-foreground">
+                    {shortDateNoTime(registration.happening.date)}
+                  </p>
+                )}
+                <div className="mt-3 flex gap-1">
+                  <Chip>{happeningTypeToString[registration.happening.type]}</Chip>
+                  <Chip>{registrationStatusToString[registration.status]}</Chip>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
       ) : (
         <p>Du er ikke påmeldt noen arrangementer.</p>
       )}
