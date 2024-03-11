4b35f04bf5cd808178d1b8bd7b31ccd769b6ce58
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-11 17:09:31 +0100

    Fix typecheck

--- apps/web/src/sanity/movies/schemas.ts
+++ apps/web/src/sanity/movies/schemas.ts
@@ -5,7 +5,7 @@ import { imageSchema } from "../utils/image";
 export const moviesSchema = z.object({
   title: z.string(),
   date: z.string(),
-  link: z.string().nullable(),
+  link: z.string(),
   image: imageSchema,
 });
 
