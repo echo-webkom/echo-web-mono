3b3d685194c07be283095a4512611fad66ce4e52
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-13 22:53:59 +0100

    Add bedpres registration to calendar (#1597)

--- apps/web/src/lib/calendar-url-builder.ts
+++ apps/web/src/lib/calendar-url-builder.ts
@@ -1,14 +1,23 @@
-const baseURL =
+const base =
   process.env.NODE_ENV === "production" ? "https://echo.uib.no" : "http://localhost:3000";
 
 export const INCLUDE_PAST_PARAM = "includePast";
 export const HAPPENING_TYPE_PARAM = "happeningType";
 export const INCLUDE_MOVIES_PARAM = "includeMovies";
+export const INCLUDE_BEDPRES_REGISTRATION_PARAM = "includeBedpresRegistration";
 
 export class CalendarUrlBuilder {
+  baseURL = base;
   includePast = false;
   happeningType: Array<string> = [];
   includeMovies = false;
+  includeBedpresRegistration = false;
+
+  constructor(baseURL: string = base) {
+    if (baseURL) {
+      this.baseURL = baseURL;
+    }
+  }
 
   setIncludePast(includePast: boolean) {
     this.includePast = includePast;
@@ -25,26 +34,31 @@ export class CalendarUrlBuilder {
     return this;
   }
 
-  build() {
-    const url = new URL(baseURL);
-    url.pathname = "/api/calendar";
+  setIncludeBedpresRegistration(includeBedpresRegistration: boolean) {
+    this.includeBedpresRegistration = includeBedpresRegistration;
+    return this;
+  }
 
-    if (this.includePast) {
-      url.searchParams.set(INCLUDE_PAST_PARAM, "true");
-    } else {
-      url.searchParams.delete(INCLUDE_PAST_PARAM);
-    }
+  build() {
+    const url = new URL(this.baseURL);
+    url.pathname = "/api/calendar";
 
     for (const type of this.happeningType) {
       url.searchParams.append(HAPPENING_TYPE_PARAM, type);
     }
 
-    if (this.includeMovies) {
-      url.searchParams.set(INCLUDE_MOVIES_PARAM, "true");
-    } else {
-      url.searchParams.delete(INCLUDE_MOVIES_PARAM);
-    }
+    this.toggleParam(url, INCLUDE_PAST_PARAM, this.includePast);
+    this.toggleParam(url, INCLUDE_MOVIES_PARAM, this.includeMovies);
+    this.toggleParam(url, INCLUDE_BEDPRES_REGISTRATION_PARAM, this.includeBedpresRegistration);
 
     return url.toString();
+  }
+
+  private toggleParam(url: URL, param: string, value: boolean) {
+    if (value) {
+      url.searchParams.set(param, "true");
+    } else {
+      url.searchParams.delete(param);
+    }
   }
 }
