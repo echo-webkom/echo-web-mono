2923c5d481e3a97d2542376bdb73040d15286b22
nikolaus <Nikolaus.Engh@student.uib.no>
2024-03-15 11:37:32 +0100

    Fixed createHappeningLink

--- apps/web/src/lib/create-link.ts
+++ apps/web/src/lib/create-link.ts
@@ -2,7 +2,7 @@ import { type HappeningType } from "@echo-webkom/lib";
 
 export const createHappeningLink = (slug: string, type: HappeningType) => {
   if (type === "bedpres") return `/bedpres/${slug}`;
-  if (type === "event") return `/arrangementer/${slug}`;
+  if (type === "event") return `/arrangement/${slug}`;
 
   return null;
 };
