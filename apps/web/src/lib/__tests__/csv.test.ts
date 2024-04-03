ba496586c5c4d93f502b4375a14146410451e04f
Herwal <herwal20@gmail.com>
2024-04-04 01:03:02 +0200

    lagde test, idk if den funka

---
+++ apps/web/src/lib/__tests__/csv.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, it } from "vitest";
+import { toCsv } from "../csv";
+import { happeningTypeEnum } from "@echo-webkom/db/schemas";
+
+
+describe("toCsv", () => {
+  it("should convert happening data to CSV format", () => {
+    const happening = {
+      registrations: [
+        {
+          user: {
+            alternativeEmail: "supah@gmail.com",
+            name: "Petter Kjellberg",
+            year: 2022,
+            degreeId: "Computer Science",
+          },
+          status: "registered",
+          unregisterReason: "",
+          answers: [
+            {
+              questionId: "q1",
+              answer: "yes",
+            },
+            {
+              questionId: "q2",
+              answer: "no",
+            },
+          ],
+        },
+      ],
+      questions: [
+        {
+          id: "q1",
+          title: "Question 1",
+        },
+        {
+          id: "q2",
+          title: "Question 2",
+        },
+      ],
+    };
+
+    const happeningData = {
+        date: null,
+        id: "",
+        type: happeningTypeEnum.enumValues[1],
+        slug: "",
+        title: "",
+        registrationGroups: null,
+        registrationStartGroups: null,
+        registrationStart: null,
+        registrationEnd: null,
+        registrations: [],
+        questions: [],
+        groups: []
+    };
+
+    const selectedHeaders: Array<string> = ["Navn", "Epost", "Status", "År", "Studieretning", "Question 1", "Question 2"];
+    const csv = toCsv(happeningData, selectedHeaders);
+
+    expect(csv).toBe(`Navn,Epost,Status,År,Studieretning,Question 1,Question 2
+  Petter Kjellberg,supah@gmail.com,registered,2022,Computer Science,yes,no`);
+  })
+});
