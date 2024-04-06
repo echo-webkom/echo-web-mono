9e4b4068744ee37d125dcb0bca5490395c5f268c
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-06 23:28:26 +0200

    Prerender events

--- apps/web/src/api/boomtown.ts
+++ apps/web/src/api/boomtown.ts
@@ -1,12 +1,11 @@
+import { BOOMTOWN_HOSTNAME, HTTP } from "@/config";
+
 import "server-only";
 
-export const BOOMTOWN_HOSTNAME = process.env.NEXT_PUBLIC_BOOMTOWN_HOSTNAME;
-
 export async function pingBoomtown(happeningId: string) {
-  return await fetch(
-    `${process.env.NODE_ENV === "production" ? "https" : "http"}://${BOOMTOWN_HOSTNAME}/${happeningId}`,
-    {
-      method: "POST",
-    },
-  ).then((response) => response.status === 200);
+  if (!BOOMTOWN_HOSTNAME) return;
+
+  return await fetch(`${HTTP}://${BOOMTOWN_HOSTNAME}/${happeningId}`, {
+    method: "POST",
+  }).then((response) => response.status === 200);
 }
--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,10 +1,19 @@
 export const PRODUCTION_DOMAIN = "echo.uib.no";
 
-export const baseURL =
-  process.env.NODE_ENV === "production"
-    ? `https://${PRODUCTION_DOMAIN}`
-    : `http://localhost:${process.env.PORT ?? 3000}`;
+export const dev = process.env.NODE_ENV !== "production";
+
+export const HTTP = dev ? "http" : "https";
+
+export const WS = dev ? "ws" : "wss";
+
+export const PORT = process.env.PORT ?? 3000;
+
+export const baseURL = dev
+  ? `http://localhost:${process.env.PORT ?? 3000}`
+  : `https://${PRODUCTION_DOMAIN}`;
 
 export const PROFILE_IMAGE_FUNCTION_URL = "https://echo-images.azurewebsites.net/api/images";
 
 export const COOKIE_BANNER = "cookie-banner";
+
+export const BOOMTOWN_HOSTNAME = process.env.NEXT_PUBLIC_BOOMTOWN_HOSTNAME;
