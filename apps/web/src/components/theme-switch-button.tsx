d35943a865f89de5cbab0ec8b094511391c04f7e
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-10 20:31:19 +0200

    Animate theme button

--- apps/web/src/components/theme-switch-button.tsx
+++ apps/web/src/components/theme-switch-button.tsx
@@ -1,20 +1,33 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { LuMoon, LuSun } from "react-icons/lu";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const [isRotating, setIsRotating] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
   const isDarkMode = theme === "dark";
 
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   function toggleTheme() {
+    setIsRotating(true);
+    setTimeout(() => setIsRotating(false), 1000);
     setTheme(isDarkMode ? "light" : "dark");
   }
 
+  const Icon = isDarkMode ? LuSun : LuMoon;
+
+  if (!isMounted) return null;
+
   return (
-    <button onClick={toggleTheme} className="h-8 w-8">
-      <LuMoon className="block h-full w-full p-1 dark:hidden" />
-      <LuSun className="hidden h-full w-full p-1 dark:block" />
+    <button onClick={toggleTheme} className={`h-8 w-8 ${isRotating ? "animate-around" : ""}`}>
+      <Icon className="block h-full w-full p-1" />
       <span className="sr-only">Toggle theme</span>
     </button>
   );
