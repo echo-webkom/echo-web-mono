ef1826bc668b336925cee015c324464b9629a9b5
Ole Magnus Fon Johnsen <me@omfj.no>
2024-04-11 21:28:56 +0200

    Refactor rss feed

--- apps/web/src/app/feed/route.ts
+++ apps/web/src/app/feed/route.ts
@@ -23,13 +23,17 @@ export async function GET() {
   const posts = await fetchAllPosts();
   const latestPostDate = posts[0] ? new Date(posts[0]._createdAt) : new Date();
 
+  const rssItems = [posts.map(postToRSSItem)]
+    .flat()
+    .sort((a, b) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime());
+
   const rssFeed = createRSSFeed({
     title: "echo – Linjeforeningen for informatikk",
     link: "https://echo.uib.no/",
     description: "Innlegg på nettsiden til echo – Linjeforeningen for informatikk",
     language: "nb",
     lastBuildDate: latestPostDate.toUTCString(),
-    items: posts.map(postToRSSItem),
+    items: rssItems,
   });
 
   return new Response(rssFeed, {
