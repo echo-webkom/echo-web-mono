f6c9ca8ff70a4f6b815a675e376bcfaf222790be
nikolaus <Nikolaus.Engh@student.uib.no>
2024-04-24 21:43:51 +0200

    formatted

--- apps/web/src/components/whitelist-button.tsx
+++ apps/web/src/components/whitelist-button.tsx
@@ -22,9 +22,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "./ui/input";
 
 const whitelistFormSchema = z.object({
-  email: z
-    .string()
-    .email("Ugyldig e-post"),
+  email: z.string().email("Ugyldig e-post"),
   days: z.coerce.number().positive("Må være et positivt tall"),
   reason: z.string().min(3, "Må være minst 3 tegn"),
 });
