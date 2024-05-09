be4925acac1da5c0826165d7608100fad94e7291
Ole Magnus Fon Johnsen <me@omfj.no>
2024-05-09 16:32:19 +0200

    Add more docs (#1893)

--- apps/web/src/lib/is-bedpres.ts
+++ apps/web/src/lib/is-bedpres.ts
@@ -1,5 +1,13 @@
 import { type fetchHomeHappenings } from "@/sanity/happening/requests";
 
+/**
+ * Type guard for checking if a happening is a bedpres.
+ *
+ * @note Asserts that the happening is a bedpres.
+ *
+ * @param happening The happening to check.
+ * @returns true if the happening is a bedpres, false otherwise.
+ */
 export const isBedpres = (
   happening: Awaited<ReturnType<typeof fetchHomeHappenings>>[number],
 ): happening is Awaited<ReturnType<typeof fetchHomeHappenings<"bedpres">>>[number] => {
