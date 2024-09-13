f87f3d28bf8e452359df8c76db7e0c7ad15f2a4d
KaroGil <karolinkagil@gmail.com>
2024-09-13 14:45:33 +0200

    change visual phd from 6 to 5+

--- apps/web/src/app/(default)/dashbord/[slug]/_components/registration-table-filters.tsx
+++ apps/web/src/app/(default)/dashbord/[slug]/_components/registration-table-filters.tsx
@@ -40,7 +40,7 @@ export const YearFilter = ({ yearFilter, setYearFilter }: YearFilterProps) => {
         <option value="3">3</option>
         <option value="4">4</option>
         <option value="5">5</option>
-        <option value="6">6</option>
+        <option value="6">5+</option>
       </Select>
     </div>
   );
