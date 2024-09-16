0281583b1935466c458e1a39a2ef8aba21293a46
nikolaus <Nikolaus.Engh@student.uib.no>
2024-09-16 23:39:55 +0200

    Removed calendar page

--- apps/web/src/app/(default)/hjem/_components/fp-calendar.tsx
+++ apps/web/src/app/(default)/hjem/_components/fp-calendar.tsx
@@ -1,9 +1,9 @@
-import { DaysCalendar as EventCalendar } from "@/components/calendar/days-calendar";
+import { Calendar } from "@/components/calendar/calendar";
 import { happeningsToCalendarEvent, moviesToCalendarEvent } from "@/lib/calendar-event-helpers";
 import { fetchAllHappenings } from "@/sanity/happening";
 import { fetchMovies } from "@/sanity/movies";
 
-export const Calendar = async ({ className }: { className?: string }) => {
+export const FPCalendar = async ({ className }: { className?: string }) => {
   const [happenings, movies] = await Promise.all([fetchAllHappenings(), fetchMovies()]);
 
   const calendarEvents = happeningsToCalendarEvent(happenings).concat(
@@ -12,7 +12,7 @@ export const Calendar = async ({ className }: { className?: string }) => {
 
   return (
     <div className={className}>
-      <EventCalendar events={calendarEvents} steps={0} isWeek />
+      <Calendar events={calendarEvents} type="week" />
     </div>
   );
 };
--- apps/web/src/components/calendar/calendar.tsx
+++ apps/web/src/components/calendar/calendar.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
+import { BiCalendar } from "react-icons/bi";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { type CalendarEvent } from "@/lib/calendar-event-helpers";
 import { Heading } from "../typography/heading";
+import { Button } from "../ui/button";
 import { CalendarControl } from "./calendar-control";
 import { DaysCalendar } from "./days-calendar";
 import { MonthCalendar } from "./month-calendar";
 
 type Props = {
   events: Array<CalendarEvent>;
+  type: "week" | "month" | "multi";
 };
 
-export const Calendar = ({ events }: Props) => {
+export const Calendar = ({ events, type }: Props) => {
   const [steps, setSteps] = useState(0);
   const [topText, setTopText] = useState("");
 
@@ -30,30 +34,57 @@ export const Calendar = ({ events }: Props) => {
   };
 
   return (
-    <div>
-      <Tabs defaultValue="week" className="w-full gap-2">
-        <div className="flex w-full gap-4">
-          <TabsList className="">
-            <TabsTrigger value="week">Ukekalender</TabsTrigger>
-            <TabsTrigger value="month">Månedskalender</TabsTrigger>
-          </TabsList>
-          <Heading level={2} className="flex-1 justify-end">
-            {topText}
-          </Heading>
-          <CalendarControl
-            prev={handlePrevStep}
-            next={handleNextStep}
-            reset={steps !== 0 ? handleReset : undefined}
-          />
+    <>
+      {type !== "multi" ? (
+        <div>
+          <div className="flex w-full gap-4">
+            <Button asChild variant="ghost">
+              <Link href="/for-studenter/arrangementer">
+                <Heading level={2}>
+                  <BiCalendar />
+                </Heading>
+              </Link>
+            </Button>
+            <Heading level={2} className="flex-1 justify-end">
+              {topText}
+            </Heading>
+            <CalendarControl
+              prev={handlePrevStep}
+              next={handleNextStep}
+              reset={steps !== 0 ? handleReset : undefined}
+            />
+          </div>
+          {type === "week" ? (
+            <DaysCalendar events={events} steps={steps} isWeek setWeekText={setTopText} />
+          ) : (
+            <MonthCalendar events={events} steps={steps} setMonthText={setTopText} />
+          )}
         </div>
+      ) : (
+        <Tabs defaultValue="week" className="w-full gap-2">
+          <div className="flex w-full gap-4">
+            <TabsList className="">
+              <TabsTrigger value="week">Ukekalender</TabsTrigger>
+              <TabsTrigger value="month">Månedskalender</TabsTrigger>
+            </TabsList>
+            <Heading level={2} className="flex-1 justify-end">
+              {topText}
+            </Heading>
+            <CalendarControl
+              prev={handlePrevStep}
+              next={handleNextStep}
+              reset={steps !== 0 ? handleReset : undefined}
+            />
+          </div>
 
-        <TabsContent value="week">
-          <DaysCalendar events={events} steps={steps} isWeek setWeekText={setTopText} />
-        </TabsContent>
-        <TabsContent value="month">
-          <MonthCalendar events={events} steps={steps} setMonthText={setTopText} />
-        </TabsContent>
-      </Tabs>
-    </div>
+          <TabsContent value="week">
+            <DaysCalendar events={events} steps={steps} isWeek setWeekText={setTopText} />
+          </TabsContent>
+          <TabsContent value="month">
+            <MonthCalendar events={events} steps={steps} setMonthText={setTopText} />
+          </TabsContent>
+        </Tabs>
+      )}
+    </>
   );
 };
