f7af8401eec6126738962d0b635fee496bb0aeb5
Ole Magnus Fon Johnsen <me@omfj.no>
2024-10-18 17:48:05 +0200

    Add parallel register test

--- apps/api/test/fixtures/happenings.ts
+++ apps/api/test/fixtures/happenings.ts
@@ -1,12 +1,39 @@
 import { addDays, subDays } from "date-fns";
 
-import { happenings } from "@echo-webkom/db/schemas";
+import { happenings, spotRanges } from "@echo-webkom/db/schemas";
 
 import { db } from "@/lib/db";
 
 export async function givenIHaveHappenings() {
-  await db.insert(happenings).values([
-    {
+  await db
+    .insert(happenings)
+    .values([
+      {
+        id: "1",
+        title: "Happening 1",
+        slug: "happening-1",
+        date: addDays(new Date(), 3),
+        registrationStart: subDays(new Date(), 2),
+        registrationEnd: addDays(new Date(), 2),
+        type: "bedpres",
+      },
+      {
+        id: "2",
+        title: "Happening 2",
+        slug: "happening-2",
+        date: new Date(),
+        registrationStart: subDays(new Date(), 3),
+        registrationEnd: subDays(new Date(), 1),
+        type: "bedpres",
+      },
+    ])
+    .onConflictDoNothing();
+}
+
+export async function givenIHaveHappeningWithTwoSpots() {
+  await db
+    .insert(happenings)
+    .values({
       id: "1",
       title: "Happening 1",
       slug: "happening-1",
@@ -14,15 +41,13 @@ export async function givenIHaveHappenings() {
       registrationStart: subDays(new Date(), 2),
       registrationEnd: addDays(new Date(), 2),
       type: "bedpres",
-    },
-    {
-      id: "2",
-      title: "Happening 2",
-      slug: "happening-2",
-      date: new Date(),
-      registrationStart: subDays(new Date(), 3),
-      registrationEnd: subDays(new Date(), 1),
-      type: "bedpres",
-    },
-  ]);
+    })
+    .onConflictDoNothing();
+
+  await db.insert(spotRanges).values({
+    happeningId: "1",
+    minYear: 1,
+    maxYear: 5,
+    spots: 1,
+  });
 }
--- apps/api/test/services/admin.test.ts
+++ apps/api/test/services/admin.test.ts
@@ -1,5 +1,6 @@
+import exp from "constants";
 import { givenIHaveComments } from "test/fixtures/comments";
-import { givenIHaveHappenings } from "test/fixtures/happenings";
+import { givenIHaveHappenings, givenIHaveHappeningWithTwoSpots } from "test/fixtures/happenings";
 import { givenIHaveUsers } from "test/fixtures/users";
 import { expect, test } from "vitest";
 
@@ -39,4 +40,40 @@ test("register on happening that has happened", async () => {
     success: false,
     message: "Påmeldingen har allerede stengt",
   });
+});
+
+test("two parallel registrations", async () => {
+  await givenIHaveUsers();
+  await givenIHaveHappeningWithTwoSpots();
+
+  const register = async (userId: string) => {
+    return await app.request("/admin/register", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer foobar",
+      },
+      body: JSON.stringify({
+        userId,
+        happeningId: "1",
+        questions: [],
+      }),
+    });
+  };
+
+  const [response1, response2] = await Promise.all([register("1"), register("2")]);
+
+  const json1 = await response1.json();
+  const json2 = await response2.json();
+
+  expect(response1.status).toBe(200);
+  expect(response2.status).toBe(200);
+  expect(json1.success).toBe(true);
+  expect(json2.success).toBe(true);
+
+  if (json1.message === "Du er nå påmeldt arrangementet") {
+    expect(json2.message).toBe("Du er nå på venteliste");
+  } else {
+    expect(json1.message).toBe("Du er nå på venteliste");
+    expect(json2.message).toBe("Du er nå påmeldt arrangementet");
+  }
 });
