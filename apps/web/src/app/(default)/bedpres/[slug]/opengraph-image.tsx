0322aecdf1ace1df1085375f7a00a9b475338125
Ole Magnus Fon Johnsen <me@omfj.no>
2024-10-18 23:32:20 +0200

    Remove og:alt

--- apps/web/src/app/(default)/bedpres/[slug]/opengraph-image.tsx
+++ apps/web/src/app/(default)/bedpres/[slug]/opengraph-image.tsx
@@ -6,7 +6,6 @@ import { fetchHappeningBySlug } from "@/sanity/happening";
 
 export const runtime = "edge";
 
-export const alt = "About Acme";
 export const size = {
   width: 1200,
   height: 630,
