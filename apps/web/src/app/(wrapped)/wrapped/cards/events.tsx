b372b4e43e4c860c4b84e02e09e723f5eeb39f76
jesperkha <hammerjesp@gmail.com>
2024-10-30 12:34:09 +0100

    event0 card and z-index for layers

--- apps/web/src/app/(wrapped)/wrapped/cards/events.tsx
+++ apps/web/src/app/(wrapped)/wrapped/cards/events.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { type WrappedCardProps, WrappedCard } from "../components/WrappedCard";
 
 export function Event0() {
@@ -12,7 +13,32 @@ export function Event0() {
   return (
     <>
       <WrappedCard props={layerProps}>
-        <p>hei</p>
+        <div className="grid w-full h-full grid-rows-2 grid-cols-1 text-3xl">
+          <div className="flex w-full h-full items-center justify-start p-10">
+            <motion.p
+              initial={{ y: 10, opacity: 0 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                transition: { ease: "easeOut", delay: 0.3 },
+              }}
+            >
+              For et hektisk år<br></br>det har vært!
+            </motion.p>
+          </div>
+          <div className="flex w-full h-full items-center flex-wrap justify-end p-10 text-white">
+            <motion.p
+              initial={{ y: 10, opacity: 0 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                transition: { ease: "easeOut", delay: 1.3 },
+              }}
+            >
+              Og med så mange<br></br>flotte arrangementer.
+            </motion.p>
+          </div>
+        </div>
       </WrappedCard>
     </>
   );
