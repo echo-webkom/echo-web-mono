fc630c16d26b38a4bfbb92f95986b8d6ffd091e2
Ole Magnus Fon Johnsen <me@omfj.no>
2024-11-05 05:11:31 +0100

    Increase connections

--- packages/db/src/serverless.ts
+++ packages/db/src/serverless.ts
@@ -2,10 +2,6 @@ import type postgres from "postgres";
 
 import { createDatabase, createPool } from "./create";
 
-const MAX_POOL = 1;
-const IDLE_TIMEOUT = 10000; // 10 seconds
-const CONNECT_TIMEOUT = 1000; // 1 seconds
-
 const globalForPool = globalThis as unknown as {
   pool: ReturnType<typeof postgres> | undefined;
 };
@@ -14,19 +10,11 @@ let pool;
 
 if (process.env.NODE_ENV !== "production") {
   if (!globalForPool.pool) {
-    globalForPool.pool = createPool({
-      max: MAX_POOL,
-      idle_timeout: IDLE_TIMEOUT,
-      connect_timeout: CONNECT_TIMEOUT,
-    });
+    globalForPool.pool = createPool();
   }
   pool = globalForPool.pool;
 } else {
-  pool = createPool({
-    max: MAX_POOL,
-    idle_timeout: IDLE_TIMEOUT,
-    connect_timeout: CONNECT_TIMEOUT,
-  });
+  pool = createPool();
 }
 
 export const db = createDatabase(pool);
