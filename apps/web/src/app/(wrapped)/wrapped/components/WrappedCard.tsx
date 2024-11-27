7d6b4b9b669a1f5223990c23a03a8c498ef25c78
Ole Magnus Fon Johnsen <me@omfj.no>
2024-11-27 15:33:17 +0100

    WIP hydration err

--- apps/web/src/app/(wrapped)/wrapped/components/WrappedCard.tsx
+++ apps/web/src/app/(wrapped)/wrapped/components/WrappedCard.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { motion, type Variants } from "framer-motion";
-import Image from "next/image";
+import { LuCircle, LuSquare, LuStar, LuTriangle } from "react-icons/lu";
 
-type ArrayOfLength<T, L extends number> =
-  | ([T, ...Array<T>] & { length: L })
-  | [];
+type ArrayOfLength<T, L extends number> = ([T, ...Array<T>] & { length: L }) | [];
 
 // C is number of layers. 0 means just foreground card for text.
 export type WrappedCardProps<C extends number> = {
@@ -28,151 +26,132 @@ function CardLayers<C extends number>({
 }) {
   const style = "absolute top-0 left-0 w-full h-full";
   return (
-    <>
-      <div className="w-full h-full">
-        <motion.div
-          className={`absolute ${props.fgColor} w-full h-full z-10 font-bold text-wrapped-black shadow overflow-hidden`}
-        >
-          {children}
-        </motion.div>
+    <div className="h-full w-full">
+      <motion.div
+        className={`absolute ${props.fgColor} text-wrapped-black z-10 h-full w-full overflow-hidden font-bold shadow`}
+      >
+        {children}
+      </motion.div>
 
-        {props.colors.map((col, index) => {
-          const variants: Variants = {
-            hidden: {
-              scale: 1,
-              rotate: 0,
-              x: 0,
-              y: 0,
+      {props.colors.map((col, index) => {
+        const variants: Variants = {
+          hidden: {
+            scale: 1,
+            rotate: 0,
+            x: 0,
+            y: 0,
+          },
+          show: {
+            rotate: props.rotate[index],
+            x: props.offX[index],
+            y: props.offY[index],
+            scale: props.scale[index],
+            transition: {
+              ease: "backOut",
             },
-            show: {
-              rotate: props.rotate[index],
-              x: props.offX[index],
-              y: props.offY[index],
-              scale: props.scale[index],
-              transition: {
-                ease: "backOut",
-              },
-            },
-          };
+          },
+        };
 
-          return (
-            <motion.div
-              style={{ zIndex: -index }}
-              key={index}
-              className={`${style} ${col} shadow`}
-              variants={variants}
-            ></motion.div>
-          );
-        })}
-      </div>
-    </>
+        return (
+          <motion.div
+            style={{ zIndex: -index }}
+            key={index}
+            className={`${style} ${col} shadow`}
+            variants={variants}
+          ></motion.div>
+        );
+      })}
+    </div>
   );
 }
 
 function CardBackdrop() {
   const length = 10 + Math.floor(Math.random() * 4);
   return (
-    <>
-      <motion.div
-        initial={{ rotate: 0 }}
-        animate={{ rotate: Math.floor(Math.random() * 20 - 10) }}
-        transition={{
-          duration: 10,
-        }}
-        className="absolute top-1/2 left-1/2 -z-50"
-      >
-        {[...new Array(length).keys()].map((_, index) => {
-          const angle = (((360 / length) * (index + 1)) / 360) * Math.PI * 2;
-          const dist = 600;
+    <motion.div
+      initial={{ rotate: 0 }}
+      animate={{ rotate: Math.floor(Math.random() * 20 - 10) }}
+      transition={{
+        duration: 10,
+      }}
+      className="absolute left-1/2 top-1/2 -z-50"
+    >
+      {[...new Array(length).keys()].map((_, index) => {
+        const angle = (((360 / length) * (index + 1)) / 360) * Math.PI * 2;
+        const dist = 600;
 
-          return (
-            <Particle
-              key={index}
-              x={Math.cos(angle) * dist}
-              y={Math.sin(angle) * dist}
-              duration={Math.random() * 3}
-            />
-          );
-        })}
-        {[...new Array(length).keys()].map((_, index) => {
-          const angle = (((360 / length) * (index + 1)) / 360) * Math.PI * 2;
-          const dist = 800;
+        return (
+          <Particle
+            key={index}
+            x={Math.cos(angle) * dist}
+            y={Math.sin(angle) * dist}
+            duration={Math.random() * 3}
+          />
+        );
+      })}
+      {[...new Array(length).keys()].map((_, index) => {
+        const angle = (((360 / length) * (index + 1)) / 360) * Math.PI * 2;
+        const dist = 800;
 
-          return (
-            <Particle
-              key={index}
-              x={Math.cos(angle) * dist}
-              y={Math.sin(angle) * dist}
-              duration={Math.random() * 2}
-            />
-          );
-        })}
-        {[...new Array(length * 2).keys()].map((_, index) => {
-          const angle =
-            (((360 / (length * 2)) * (index + 1)) / 360) * Math.PI * 2;
-          const dist = 1000;
+        return (
+          <Particle
+            key={index}
+            x={Math.cos(angle) * dist}
+            y={Math.sin(angle) * dist}
+            duration={Math.random() * 2}
+          />
+        );
+      })}
+      {[...new Array(length * 2).keys()].map((_, index) => {
+        const angle = (((360 / (length * 2)) * (index + 1)) / 360) * Math.PI * 2;
+        const dist = 1000;
 
-          return (
-            <Particle
-              key={index}
-              x={Math.cos(angle) * dist}
-              y={Math.sin(angle) * dist}
-              duration={Math.random() * 3}
-            />
-          );
-        })}
-      </motion.div>
-    </>
+        return (
+          <Particle
+            key={index}
+            x={Math.cos(angle) * dist}
+            y={Math.sin(angle) * dist}
+            duration={Math.random() * 3}
+          />
+        );
+      })}
+    </motion.div>
   );
 }
+const icons = [LuStar, LuCircle, LuSquare, LuTriangle];
 
-function Particle({
-  x,
-  y,
-  duration,
-}: {
-  x: number;
-  y: number;
-  duration: number;
-}) {
-  const images = ["star", "circle", "square", "triangle"];
+function Particle({ x, y, duration }: { x: number; y: number; duration: number }) {
+  const Icon = icons[Math.floor(Math.random() * icons.length)]!;
+
   return (
-    <>
-      <motion.div
-        className="absolute w-10 h-10"
-        initial={{
-          x: 0,
-          y: 0,
-          rotate: 0,
-          opacity: 0,
-        }}
-        animate={{
-          x: [Math.floor(x / 1.5), Math.floor(x)],
-          y: [Math.floor(y / 1.5), Math.floor(y)],
-          opacity: [0, 0.1, 1, 0],
-          rotate: 360,
-        }}
-        exit={{
-          opacity: [null, 0],
-          transition: {
-            duration: 0.05,
-          },
-        }}
-        transition={{
-          duration: 7 + duration,
-          times: [0, 0.05, 0.9, 1],
-          ease: "linear",
-        }}
-      >
-        <Image
-          src={`/wrapped/${images[Math.floor(Math.random() * images.length)]}.png`}
-          alt=""
-          className="opacity-20"
-          width={500}
-          height={500}
-        />
-      </motion.div>
-    </>
+    <motion.div
+      className="absolute h-10 w-10"
+      initial={{
+        x: 0,
+        y: 0,
+        rotate: 0,
+        opacity: 0,
+      }}
+      animate={{
+        x: [Math.floor(x / 1.5), Math.floor(x)],
+        y: [Math.floor(y / 1.5), Math.floor(y)],
+        opacity: [0, 0.1, 1, 0],
+        rotate: 360,
+      }}
+      exit={{
+        opacity: [null, 0],
+        transition: {
+          duration: 0.05,
+        },
+      }}
+      transition={{
+        duration: 7 + duration,
+        times: [0, 0.05, 0.9, 1],
+        ease: "linear",
+      }}
+    >
+      <Icon className="h-10 w-10 opacity-20" />
+    </motion.div>
   );
 }
 
@@ -203,23 +182,21 @@ export function WrappedCard<C extends number>({
   };
 
   return (
-    <>
-      <div
-        className={`relative top-0 left-0 w-[100vw] h-[100vh] overflow-hidden -z-50 ${props.bgColor}`}
-      >
-        <div className="flex w-full h-full justify-center items-center overflow-hidden">
-          <motion.div
-            initial="hidden"
-            animate="show"
-            exit="exit"
-            variants={mainVariants}
-            className="select-none relative w-[30vw] h-[75vh]"
-          >
-            {!props.noParticles && <CardBackdrop />}
-            <CardLayers props={props}>{children}</CardLayers>
-          </motion.div>
-        </div>
+    <div
+      className={`relative left-0 top-0 -z-50 h-[100vh] w-[100vw] overflow-hidden ${props.bgColor}`}
+    >
+      <div className="flex h-full w-full items-center justify-center overflow-hidden">
+        <motion.div
+          initial="hidden"
+          animate="show"
+          exit="exit"
+          variants={mainVariants}
+          className="relative h-[75vh] w-[30vw] select-none"
+        >
+          {!props.noParticles && <CardBackdrop />}
+          <CardLayers props={props}>{children}</CardLayers>
+        </motion.div>
       </div>
-    </>
+    </div>
   );
 }
