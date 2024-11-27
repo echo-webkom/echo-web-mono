e6a8b6a06e1dbe2cb0c33f341b8f72394fa61eba
Ole Magnus <me@omfj.no>
2024-11-27 14:42:14 +0100

    Discard changes to apps/web/src/app/(default)/layout.tsx

--- apps/web/src/app/(default)/layout.tsx
+++ apps/web/src/app/(default)/layout.tsx
@@ -2,25 +2,17 @@ import { type ReactNode } from "react";
 
 import { Footer } from "@/components/footer";
 import { SiteHeader } from "@/components/site-header";
-import { AnimatedIcons } from "@/components/animations/animated-icons";
 
 export type DefaultLayoutProps = {
   children: ReactNode;
 };
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
-  const isOctober = new Date().getMonth() === 9;
-
   return (
-    <AnimatedIcons n={40}>
-      <div
-        className="flex min-h-screen flex-col"
-        data-theme={isOctober ? "halloween" : "default"}
-      >
-        <SiteHeader />
-        <div className="flex w-full flex-grow flex-col">{children}</div>
-        <Footer />
-      </div>
-    </AnimatedIcons>
+    <div className="flex min-h-screen flex-col">
+      <SiteHeader />
+      <div className="flex w-full flex-grow flex-col">{children}</div>
+      <Footer />
+    </div>
   );
 }
