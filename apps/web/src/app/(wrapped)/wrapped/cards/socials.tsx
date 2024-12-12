474d107cc5dd57097a3bde4d83302ebab1672f09
jesperkha <hammerjesp@gmail.com>
2024-12-12 18:02:09 +0100

    finished interaction card

--- apps/web/src/app/(wrapped)/wrapped/cards/socials.tsx
+++ apps/web/src/app/(wrapped)/wrapped/cards/socials.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { FaRegThumbsUp, FaReply } from "react-icons/fa";
+import { SlSpeech } from "react-icons/sl";
 
 import { AppearingText } from "../components/Text";
 import { WrappedCard, type WrappedCardProps } from "../components/WrappedCard";
@@ -55,19 +57,58 @@ export const InteractionCard = () => {
 
   return (
     <WrappedCard props={layerProps}>
-      <div className="flex h-full w-full flex-col items-center justify-center gap-10 p-10">
-        <div className="flex w-full items-center gap-5">
-          <div className="h-full"></div>
-          <p>{COMMENTS} kommentarer</p>
-        </div>
-        <div className="flex w-full items-center gap-5">
-          <div className="h-full"></div>
-          <p>{REPLIES} replies</p>
-        </div>
-        <div className="flex w-full items-center gap-5">
-          <div className="h-full"></div>
-          <p>{REACTIONS} reaksjoner</p>
-        </div>
+      <div className="flex h-full w-full flex-col p-20 text-2xl">
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, delay: 0.5 }}
+          className="grid h-1/3 w-full grid-cols-3 grid-rows-2"
+        >
+          <div className="col-span-1 row-span-2 flex items-center justify-center">
+            <SlSpeech className="h-1/2 w-1/2" />
+            <div className="bg-wrapped-blue absolute -z-10 h-32 w-32 rounded-full"></div>
+          </div>
+          <div className="col-span-2 row-span-1 flex items-end justify-center p-2">
+            <p className="text-wrapped-black text-5xl">{COMMENTS}</p>
+          </div>
+          <div className="col-span-2 row-span-1 flex items-start justify-center p-2">
+            <p className="text-wrapped-grey opacity-50">kommentarer</p>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, delay: 1 }}
+          className="grid h-1/3 w-full grid-cols-3 grid-rows-2"
+        >
+          <div className="col-span-1 row-span-2 flex items-center justify-center">
+            <FaReply className="h-1/2 w-1/2" />
+            <div className="bg-wrapped-pink absolute -z-10 h-32 w-32 rounded-full"></div>
+          </div>
+          <div className="col-span-2 row-span-1 flex items-end justify-center p-2">
+            <p className="text-wrapped-black text-5xl">{REPLIES}</p>
+          </div>
+          <div className="col-span-2 row-span-1 flex items-start justify-center p-2">
+            <p className="text-wrapped-grey opacity-50">replies</p>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, delay: 1.5 }}
+          className="grid h-1/3 w-full grid-cols-3 grid-rows-2"
+        >
+          <div className="col-span-1 row-span-2 flex items-center justify-center">
+            <FaRegThumbsUp className="h-1/2 w-1/2" />
+            <div className="bg-wrapped-yellow absolute -z-10 h-32 w-32 rounded-full"></div>
+          </div>
+          <div className="col-span-2 row-span-1 flex items-end justify-center p-2">
+            <p className="text-wrapped-black text-5xl">{REACTIONS}</p>
+          </div>
+          <div className="col-span-2 row-span-1 flex items-start justify-center p-2">
+            <p className="text-wrapped-grey opacity-50">reaksjoner</p>
+          </div>
+        </motion.div>
       </div>
     </WrappedCard>
   );
