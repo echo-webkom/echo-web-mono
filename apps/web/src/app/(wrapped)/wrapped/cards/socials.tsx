75ca130ec147504b489abcc7d786ea1126b70c66
Ole Magnus Fon Johnsen <me@omfj.no>
2024-12-19 02:46:12 +0100

    Use motion
    Framer motion har blitt til -> motion

--- apps/web/src/app/(wrapped)/wrapped/cards/socials.tsx
+++ apps/web/src/app/(wrapped)/wrapped/cards/socials.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { FaRegThumbsUp, FaReply } from "react-icons/fa";
 import { SlSpeech } from "react-icons/sl";
 
--- apps/web/src/app/(wrapped)/wrapped/page.tsx
+++ apps/web/src/app/(wrapped)/wrapped/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 
 import {
   AgendaEvent,
