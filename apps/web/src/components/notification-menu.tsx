356e3814852ee483c4290fcaae2961a671ba5f54
Izaak Sarnecki <izaaksarnecki@outlook.com>
2024-12-26 23:29:21 +0100

    try to fetch notifs from db

--- apps/web/src/components/notification-menu.tsx
+++ apps/web/src/components/notification-menu.tsx
@@ -1,6 +1,9 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { RxBell as Bell } from "react-icons/rx";
 
-import { fetchNotifications } from "@/sanity/notification/requests";
+import { getNotifications } from "@/data/notifications/queries";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,8 +13,30 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
-export const NotificationMenu = async () => {
-  const notifications = await fetchNotifications();
+type Notification = {
+  id: string;
+  title: string;
+  dateFrom: string;
+  dateTo: string;
+};
+
+export const NotificationMenu = () => {
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getNotifications();
+      setNotifications(
+        result.map((notification) => ({
+          id: notification.id,
+          title: notification.name,
+          dateFrom: notification.dateFrom,
+          dateTo: notification.dateTo,
+        })),
+      );
+    };
+    fetchData();
+  }, []);
 
   return (
     <DropdownMenu>
@@ -27,7 +52,7 @@ export const NotificationMenu = async () => {
           <DropdownMenuItem>Du har ingen notifikasjoner!</DropdownMenuItem>
         ) : (
           notifications.map((notification) => (
-            <DropdownMenuItem key={notification._id}>
+            <DropdownMenuItem key={notification.id}>
               <DropdownMenuLabel className="truncate">{notification.title}</DropdownMenuLabel>
             </DropdownMenuItem>
           ))
