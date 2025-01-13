6d9ab864429ce9655de89d5d84096c06827bfaf2
jesperkha <hammerjesp@gmail.com>
2025-01-13 11:58:55 +0100

    user context

---
+++ apps/web/src/app/(wrapped)/wrapped/components/UserContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext } from "react";
+
+import { getUser } from "@/lib/get-user";
+
+export type UserWrappedData = {
+  comments: number;
+  replies: number;
+  registrations: number;
+  registeredRegistrations: number;
+  fastestRegistration: number | null;
+  registrationPrecentile: number | null;
+  reactions: number;
+};
+
+const defaultUserWrappedData: UserWrappedData = {
+  comments: 0,
+  replies: 0,
+  registrations: 0,
+  registeredRegistrations: 0,
+  fastestRegistration: null,
+  registrationPrecentile: null,
+  reactions: 0,
+};
+
+const UserContext = createContext<UserWrappedData | null>(null);
+
+const fetchUserData = async (): Promise<UserWrappedData | null> => {
+  const user = await getUser();
+
+  if (user === null) {
+    return null;
+  }
+
+  const resp = await fetch(`https://echo-wrapped-stats.fly.dev/stats/${user.id}`, {
+    headers: {
+      Authorization: process.env.ADMIN_KEY || "",
+    },
+  });
+
+  if (!resp.ok) {
+    return null;
+  }
+
+  return (await resp.json()) as UserWrappedData;
+};
+
+export const UserProvider = async ({ children }: { children: React.ReactNode }) => {
+  let data = await fetchUserData();
+  if (data === null) data = defaultUserWrappedData;
+
+  return (
+    <>
+      <UserContext.Provider value={data}>{children}</UserContext.Provider>
+    </>
+  );
+};
