8b9c3d0c46f198f958096c75e94a8d9de320a3a0
Ole Magnus <me@omfj.no>
2025-01-29 18:04:38 +0100

    Show registrations 2 (#2442)

--- apps/web/src/components/happening/happening-sidebar.tsx
+++ apps/web/src/components/happening/happening-sidebar.tsx
@@ -31,8 +31,9 @@ import {
 } from "@/utils/date";
 import { doesIntersect } from "@/utils/list";
 import { mailTo } from "@/utils/prefixes";
-import { ReactionButtonGroup } from "./reaction-button-group";
-import { RegistrationCount } from "./registration-count";
+import { ReactionButtonGroup } from "../reaction-button-group";
+import { RegistrationCount } from "../registration-count";
+import { RegistrationsPreview } from "./registrations-preview";
 
 type EventSidebarProps = {
   event: Exclude<Awaited<ReturnType<typeof fetchHappeningBySlug>>, null>;
@@ -486,6 +487,18 @@ export const HappeningSidebar = async ({ event }: EventSidebarProps) => {
           )}
         </div>
       </Sidebar>
+
+      {Boolean(user) && (
+        <RegistrationsPreview
+          registrations={registrations.map((registration) => ({
+            image: registration.user.image,
+            name: registration.user.name,
+            userId: registration.userId,
+            status: registration.status,
+          }))}
+        />
+      )}
+
       {/**
        * Show link to admin dashbord if:
        * - User is host
---
+++ apps/web/src/components/happening/registrations-preview.tsx
@@ -0,0 +1,103 @@
+import { type RegistrationStatus } from "@echo-webkom/db/schemas";
+
+import { ellipsis, initials } from "@/utils/string";
+import { Text } from "../typography/text";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+
+type Registration = {
+  image: string | null;
+  name: string | null;
+  userId: string;
+  status: RegistrationStatus;
+};
+
+type ProfilePreviewProps = {
+  registration: Registration;
+};
+
+const ProfilePreview = ({ registration }: ProfilePreviewProps) => {
+  const fallback = initials(registration.name ?? "BO");
+
+  return (
+    <Avatar className="size-8">
+      <AvatarImage src={registration.image ?? ""} />
+      <AvatarFallback>{fallback}</AvatarFallback>
+    </Avatar>
+  );
+};
+
+type RegistrationsPreviewProps = {
+  registrations: Array<Registration>;
+};
+
+const MAX = 7;
+
+export const RegistrationsPreview = ({ registrations }: RegistrationsPreviewProps) => {
+  const sorted = registrations
+    .filter((registration) => registration.status === "registered")
+    .sort((a, b) => {
+      if (a.image && !b.image) {
+        return -1;
+      } else if (!a.image && b.image) {
+        return 1;
+      }
+      return 0;
+    })
+    .slice(0, MAX);
+
+  if (sorted.length < 3) {
+    return null;
+  }
+
+  const extra = registrations.length - MAX;
+  const names = `${sorted.map((registration) => ellipsis(registration.name ?? "", 7)).join(", ")}${
+    extra > 0 ? ` +${extra}` : ""
+  }`;
+
+  return (
+    <Dialog>
+      <DialogTrigger className="group flex w-fit items-start">
+        <TooltipProvider>
+          <Tooltip>
+            <div>
+              <TooltipTrigger>
+                <div className="flex items-center">
+                  <div className="flex items-center -space-x-4">
+                    {sorted.map((registration) => (
+                      <ProfilePreview key={registration.userId} registration={registration} />
+                    ))}
+                  </div>
+                  {extra > 0 && (
+                    <div className="ml-2 text-sm font-medium text-gray-600 group-hover:underline">
+                      +{extra}
+                    </div>
+                  )}
+                </div>
+              </TooltipTrigger>
+            </div>
+            <TooltipContent>
+              <p>{names}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Påmeldte brukere</DialogTitle>
+        </DialogHeader>
+        <div className="px-4 pb-4">
+          {registrations
+            .filter((registration) => registration.status === "registered")
+            .map((registration) => (
+              <div key={registration.userId} className="flex items-center space-x-4">
+                <ProfilePreview registration={registration} />
+                <Text className="text-muted-foreground">{registration.name}</Text>
+              </div>
+            ))}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
