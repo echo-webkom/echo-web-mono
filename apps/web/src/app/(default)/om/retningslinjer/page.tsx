a938e56eec1ae5a02027bea5f2e699c325c3d82d
Ole Magnus Fon Johnsen <me@omfj.no>
2025-04-01 18:08:52 +0200

    Remove title from etiskeretningslinjer

--- apps/web/src/app/(default)/om/retningslinjer/page.tsx
+++ apps/web/src/app/(default)/om/retningslinjer/page.tsx
@@ -2,8 +2,6 @@ import { type Metadata } from "next";
 
 import { Container } from "@/components/container";
 import { Markdown } from "@/components/markdown";
-import { Heading } from "@/components/typography/heading";
-import { Text } from "@/components/typography/text";
 import { StaticPageSidebar } from "@/lib/static-page-sidebar";
 
 export const metadata = {
@@ -26,22 +24,6 @@ export default async function EthicalGuidelines() {
       <StaticPageSidebar />
 
       <div className="space-y-8">
-        <div>
-          <Heading>Etiske retningslinjer</Heading>
-          <Text>
-            De etiske retningslinjene til echo – Linjeforeningen for informatikk. Du kan og lese de
-            på{" "}
-            <a
-              className="font-medium underline transition-colors duration-200 after:content-['_↗'] hover:text-blue-500"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/echo-uib/Retningslinjer/blob/main/Etiske_retningslinjer.md"
-            >
-              GitHub
-            </a>
-            .
-          </Text>
-        </div>
         <Markdown content={markdown} />
       </div>
     </Container>
