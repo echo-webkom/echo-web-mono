3e36e40b7de8e451ffc21889b3dac913dd3821ec
Ole Magnus Fon Johnsen <me@omfj.no>
2025-04-02 18:42:09 +0200

    Fix typo

--- apps/web/src/components/calendar/event-hover-prev.tsx
+++ apps/web/src/components/calendar/event-hover-prev.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import { format } from "date-fns";
 
 import { type CalendarEvent } from "@/lib/calendar-event-helpers";
+import { time } from "@/utils/date";
 
 type Props = {
   event: CalendarEvent;
@@ -15,7 +15,7 @@ export const EventHoverPreview = ({ event }: Props) => {
         <Link className="hover:underline" href={event.link}>
           <h3 className="line-clamp-1 text-ellipsis font-semibold">{event.title}</h3>
         </Link>
-        <h3 className="text-sm">{format(event.date, "HH:mm")}</h3>
+        <h3 className="text-sm">{time(event.date)}</h3>
       </div>
       <p className="text-sm font-medium">
         {event.body.slice(0, 250)}
