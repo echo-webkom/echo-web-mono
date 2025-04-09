bf8b7b16038a34f93a9a27609f1e32725afde2a8
Ole Magnus Fon Johnsen <me@omfj.no>
2025-04-09 10:23:20 +0200

    Fix endpoint method

--- apps/api/src/services/strikes.ts
+++ apps/api/src/services/strikes.ts
@@ -8,7 +8,7 @@ import { admin } from "@/middleware/admin";
 
 const app = new Hono();
 
-app.get("/strikes/unban", admin(), async (c) => {
+app.post("/strikes/unban", admin(), async (c) => {
   await db.delete(dots).where(lte(dots.expiresAt, new Date()));
   await db.delete(banInfos).where(lte(banInfos.expiresAt, new Date()));
 
