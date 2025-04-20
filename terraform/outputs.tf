65c652281b6f0db81cc1b85ce6dfe704d6ba0ac2
Ole Magnus Fon Johnsen <me@omfj.no>
2025-04-20 22:44:47 +0200

    Remove db_url from outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,9 +7,3 @@ output "axis_url" {
   description = "The URL of the Axis web app"
   value       = azurerm_linux_web_app.echo-vertex.default_hostname
 }
-
-output "db_url" {
-  description = "The connection string for the PostgreSQL database"
-  value       = azurerm_postgresql_flexible_server.db.fqdn
-  sensitive   = true
-}
