a01e88bfa48c7040d027b96cb0d24e466ce65abd
KaroGil <karolinkagil@gmail.com>
2025-04-30 19:22:43 +0200

    add pizzaformel to event page

---
+++ apps/web/src/components/pizza-formel.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  registration_count: number;
+};
+
+export function PizzaFormel({ registration_count }: Props) {
+  const pizza_count = registration_count * (3 / 8);
+  return <p>Du må bestille: {pizza_count}</p>;
+}
