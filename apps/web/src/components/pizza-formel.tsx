1268bacde68ad76a8659a3d71c24a93b15626e52
KaroGil <karolinkagil@gmail.com>
2025-05-01 21:13:00 +0200

    add pizzaformel to the dashboard

--- apps/web/src/components/pizza-formel.tsx
+++ apps/web/src/components/pizza-formel.tsx
@@ -3,6 +3,16 @@ type Props = {
 };
 
 export function PizzaFormel({ registration_count }: Props) {
+  if (registration_count < 0) {
+    return <p>Antall kan ikke være negativt</p>;
+  }
+  if (registration_count === 0) {
+    return <p>Ingen pizza</p>;
+  }
+  if (Number.isNaN(registration_count)) {
+    return <p>Input må være et tall</p>;
+  }
+
   const pizza_count = registration_count * (3 / 8);
   return <p>Du må bestille: {pizza_count} pizza</p>;
 }
