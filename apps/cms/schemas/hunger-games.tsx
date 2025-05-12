16ad41ee77b2e1f59fd7d783840f2e4c99b4e616
Zeno Elio Leonardi <145581496+0Zeno@users.noreply.github.com>
2025-05-12 11:31:23 +0200

    Add preview to hungergames in sanity (#2498)

--- apps/cms/schemas/hunger-games.tsx
+++ apps/cms/schemas/hunger-games.tsx
@@ -1,25 +1,29 @@
 import { defineField, defineType } from "sanity";
 
 export default defineType({
-    name: "hungerGames",
-    title: "Hunger Games",
-    type: "document",
-    fields: [
-        defineField({
-            name: "profile",
-            title: "Profil",
-            type: "reference",
-            to: { type: "profile" },
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: "isDead",
-            title: "Er død?",
-            type: "boolean",
-            
-        })
-    ],
-    initialValue: {
-        isDead: false
-    }
-})
+  name: "hungerGames",
+  title: "Hunger Games",
+  type: "document",
+  preview: {
+    select: {
+      title: "profile.name",
+    },
+  },
+  fields: [
+    defineField({
+      name: "profile",
+      title: "Profil",
+      type: "reference",
+      to: { type: "profile" },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "isDead",
+      title: "Er død?",
+      type: "boolean",
+    }),
+  ],
+  initialValue: {
+    isDead: false,
+  },
+});
