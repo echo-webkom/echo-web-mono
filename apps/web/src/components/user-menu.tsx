eb6a1eb2305c1672faae4280e6b13ba6f4f22c8c
Herwal <herwal20@gmail.com>
2025-09-03 13:15:43 +0200

    layout routing

--- apps/web/src/components/user-menu.tsx
+++ apps/web/src/components/user-menu.tsx
@@ -63,7 +63,7 @@ export const UserMenu = ({ user }: UserMenuProps) => {
         <DropdownMenuSeparator />
 
         <DropdownMenuItem asChild>
-          <Link href="/auth/profil">
+          <Link href={`auth/user/${user.id}`}>
             <Person className="mr-2 h-4 w-4" />
             <span>Min profil</span>
           </Link>
