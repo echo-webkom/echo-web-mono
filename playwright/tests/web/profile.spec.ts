2702be077cfd1227bb22b7edbe31123501f8e8a4
Herwal <herwal20@gmail.com>
2025-09-04 12:14:02 +0200

    test test

--- playwright/tests/web/profile.spec.ts
+++ playwright/tests/web/profile.spec.ts
@@ -18,15 +18,6 @@ test("update profile", async ({ page }) => {
   await expect(page.getByTestId("toast").getByText("Brukeren ble oppdatert")).toBeVisible();
 });
 
-test("can't see private profile", async ({ page }) => {
-  await loginAs(page, "Student");
-
-  await page.goto("/auth/user/student2");
-
-  await expect(page.getByText("Student2", { exact: true })).toBeVisible();
-  await expect(page.getByText("Denne brukeren har privat profil.")).toBeVisible();
-});
-
 test("see admin dashboard", async ({ page }) => {
   await loginAs(page, "Admin");
 
@@ -41,4 +32,10 @@ test("see admin dashboard", async ({ page }) => {
   await dashboardItem.click();
 
   await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+});
+
+test("can't see private profile", async ({ page }) => {
+  await loginAs(page, "Student");
+  await page.goto("/auth/user/student2");
+  await expect(page.getByText("Denne brukeren har privat profil.")).toBeVisible();
 });
