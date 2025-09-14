6a15e317b50378fc1a1a5dd98cc076c214dada6a
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-14 14:17:26 +0200

    feat(types): add explicit type exports for asset modules

--- apps/web/src/declarations.d.ts
+++ apps/web/src/declarations.d.ts
@@ -1,6 +1,29 @@
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
-declare module "*.svg";
-declare module "*.wav";
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.wav" {
+  const content: string;
+  export default content;
+}
