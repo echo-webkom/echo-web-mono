8d0b61c12b02cee44ca26cebad60822acd114f91
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-15 10:09:00 +0200

    Update posthog config

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -4,7 +4,6 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
   ui_host: "https://eu.posthog.com",
   defaults: "2025-05-24",
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  persistence: "memory",
   debug: process.env.NODE_ENV === "development",
+  cookieless_mode: "always",
 });
