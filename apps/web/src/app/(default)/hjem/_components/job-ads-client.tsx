94f00464c6a93781f2fe9392af653cb2e1eccff0
Johanne Blikberg Herheim <145448180+johanneherheim@users.noreply.github.com>
2025-09-15 15:39:27 +0200

    Make bedpres company ads responsive (#2706)

--- apps/web/src/app/(default)/hjem/_components/job-ads-client.tsx
+++ apps/web/src/app/(default)/hjem/_components/job-ads-client.tsx
@@ -7,7 +7,7 @@ import { type JobAdsQueryResult } from "@echo-webkom/cms/types";
 
 import { JobAdPreview } from "@/components/job-ad-preview";
 
-export const JobAdCarouselClient = ({ jobAds }: { jobAds: Array<JobAdsQueryResult[number]> }) => {
+export const JobAdCarousel = ({ jobAds }: { jobAds: Array<JobAdsQueryResult[number]> }) => {
   const [page, setPage] = useState(0);
   const adsPerPage = 2;
 
@@ -18,32 +18,44 @@ export const JobAdCarouselClient = ({ jobAds }: { jobAds: Array<JobAdsQueryResul
   const hasNext = start + adsPerPage < jobAds.length;
 
   return (
-    <div className="relative flex items-center">
-      {hasPrev && (
-        <button
-          onClick={() => setPage((p) => p - 1)}
-          className="absolute left-[-25px] z-10 rounded-md border bg-background p-2"
-        >
-          <ArrowLeft />
-        </button>
-      )}
-
-      <ul className="flex gap-2">
-        {currentAds.map((jobAd) => (
-          <li key={jobAd._id} className="flex-1">
+    <>
+      {/* Mobil */}
+      <ul className="flex snap-x snap-mandatory gap-5 overflow-x-auto sm:hidden">
+        {jobAds.map((jobAd) => (
+          <li key={jobAd._id} className="min-w-[90%] snap-center">
             <JobAdPreview jobAd={jobAd} hideBorder={true} />
           </li>
         ))}
       </ul>
 
-      {hasNext && (
-        <button
-          onClick={() => setPage((p) => p + 1)}
-          className="absolute right-[-25px] z-10 rounded-md border bg-background p-2"
-        >
-          <ArrowRight />
-        </button>
-      )}
-    </div>
+      {/* Desktop */}
+      <div className="relative hidden items-center sm:flex">
+        {hasPrev && (
+          <button
+            onClick={() => setPage((p) => p - 1)}
+            className="absolute left-[-25px] z-10 rounded-md border bg-background p-2"
+          >
+            <ArrowLeft />
+          </button>
+        )}
+
+        <ul className="flex gap-2">
+          {currentAds.map((jobAd) => (
+            <li key={jobAd._id} className="flex-1">
+              <JobAdPreview jobAd={jobAd} hideBorder={true} />
+            </li>
+          ))}
+        </ul>
+
+        {hasNext && (
+          <button
+            onClick={() => setPage((p) => p + 1)}
+            className="absolute right-[-25px] z-10 rounded-md border bg-background p-2"
+          >
+            <ArrowRight />
+          </button>
+        )}
+      </div>
+    </>
   );
 };
