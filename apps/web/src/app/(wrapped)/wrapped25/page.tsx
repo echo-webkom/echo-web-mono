1320df0c7c5d44f6e014e65d6193233280569766
jesper <hammerjesp@gmail.com>
2025-10-07 13:49:13 +0200

    Add Scroller and old stats

--- apps/web/src/app/(wrapped)/wrapped25/page.tsx
+++ apps/web/src/app/(wrapped)/wrapped25/page.tsx
@@ -1,5 +1,21 @@
+import Scroller from "./Scroller";
+
+function Red() {
+  return <div className="h-full w-full bg-red-500">Red Screen</div>;
+}
+
+function Blue() {
+  return <div className="h-full w-full bg-blue-500">Blue Screen</div>;
+}
+
+function Green() {
+  return <div className="h-full w-full bg-green-500">Green Screen</div>;
+}
+
 export default function Wrapped() {
-    return <>
-        <p className="text-5xl">Hello</p>
+  return (
+    <>
+      <Scroller slides={[<Red />, <Blue />, <Green />]}></Scroller>
     </>
+  );
 }
