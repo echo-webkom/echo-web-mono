d495f73cfaba55150324cf3a1ac9d92388c28ddf
Githan Mayurathan <gith@n.mayurathan@gmail.com>
2025-10-08 17:42:43 +0200

    La til TextTyped komponent

--- apps/web/src/app/(wrapped)/wrapped25/page.tsx
+++ apps/web/src/app/(wrapped)/wrapped25/page.tsx
@@ -1,3 +1,4 @@
+import TextType from "./components/TextType";
 import Scroller from "./Scroller";
 
 function Red() {
@@ -12,10 +13,30 @@ function Green() {
   return <div className="h-full w-full bg-green-500">Green Screen</div>;
 }
 
+function Welcome() {
+  return (
+    <>
+      <TextType
+        text={["Endelig echo wrapped 2025", "Endelig echo wrapped 2025"]}
+        typingSpeed={100}
+        pauseDuration={1500}
+        showCursor={true}
+        cursorCharacter="|"
+      />
+      <div>
+        <br />
+        <br />
+        <br />
+        <h1>Velkommen</h1>
+      </div>
+    </>
+  );
+}
+
 export default function Wrapped() {
   return (
     <>
-      <Scroller slides={[<Red />, <Blue />, <Green />]}></Scroller>
+      <Scroller slides={[<Red />, <Blue />, <Welcome />]}></Scroller>
     </>
   );
 }
