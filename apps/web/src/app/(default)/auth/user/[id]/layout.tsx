bb36c514a66402c173f818c34f2a3b670e636407
Herwal <herwal20@gmail.com>
2025-10-08 17:32:41 +0200

    yes

--- apps/web/src/app/(default)/auth/user/[id]/layout.tsx
+++ apps/web/src/app/(default)/auth/user/[id]/layout.tsx
@@ -27,7 +27,7 @@ export default async function ProfileLayout(props: LayoutProps<"/auth/user/[id]"
     return null;
   }
 
-  const profileOwnerId = (await params).id as string;
+  const profileOwnerId = String((await params).id);
   const routes = getRoutes(profileOwnerId);
 
   if (currentUser.id !== profileOwnerId) {
