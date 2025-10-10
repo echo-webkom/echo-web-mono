34168b72677a2b9903f4332f0f2a4caadfd006cb
Ole Magnus Fon Johnsen <me@omfj.no>
2025-10-10 20:00:43 +0200

    chore: remove uno

--- apps/uno/pkg/middleware/logger.go
+++
@@ -1,7 +0,0 @@
-package middleware
-
-import "github.com/go-chi/chi/v5/middleware"
-
-func Logger() Middleware {
-	return middleware.Logger
-}
--- apps/uno/pkg/middleware/middleware.go
+++
@@ -1,7 +0,0 @@
-package middleware
-
-import (
-	"net/http"
-)
-
-type Middleware func(http.Handler) http.Handler
