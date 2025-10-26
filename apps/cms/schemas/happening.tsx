4553ded1040e21c6430f5233f1d579c9d5e580ab
Ole Magnus Fon Johnsen <me@omfj.no>
2025-10-26 15:37:51 +0100

    docs: update location description

--- apps/cms/schemas/happening.tsx
+++ apps/cms/schemas/happening.tsx
@@ -131,6 +131,7 @@ export default defineType({
       name: "location",
       title: "Sted",
       type: "reference",
+      description: "Hvor arrangementet skal finne sted. Arrangementer på 'Programmerbar' vil også vises på programmer.bar.",
       to: { type: "location" },
     }),
     defineField({
