1968babcd3b4e578d53ea4c6edfb518b8978e90d
jesperkha <hammerjesp@gmail.com>
2025-10-29 16:08:07 +0100

    Add bootstrap

---
+++ apps/uno/bootstrap/api.go
@@ -0,0 +1,29 @@
+package bootstrap
+
+import (
+	"log"
+	"os"
+	"syscall"
+	"uno/apps/api"
+	"uno/config"
+	"uno/data/database"
+	"uno/data/repo"
+
+	"github.com/jesperkha/notifier"
+)
+
+func RunApi() {
+	config := config.Load()
+	notif := notifier.New()
+
+	db, err := database.New(config.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := repo.New(db.Pool)
+
+	go api.Run(notif, config, repo)
+
+	notif.NotifyOnSignal(syscall.SIGINT, os.Interrupt)
+}
