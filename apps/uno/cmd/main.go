4a8c66fe9025c88bea0e5917cfe3478c82277d09
jesperkha <hammerjesp@gmail.com>
2025-10-29 16:08:07 +0100

    Add bootstrap

--- apps/uno/cmd/main.go
+++ apps/uno/cmd/main.go
@@ -1,32 +1,7 @@
-package cmd
+package main
 
-import (
-	"log"
-	"os"
-	"syscall"
-	"uno/apps/api"
-	"uno/config"
-	"uno/data/database"
-	"uno/data/repo"
-
-	"github.com/jesperkha/notifier"
-)
+import "uno/bootstrap"
 
 func main() {
-	log.Println("Starting UNO")
-
-	config := config.Load()
-	notif := notifier.New()
-
-	db, err := database.New(config.DatabaseURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	repo := repo.New(db.Pool)
-
-	go api.Run(notif, config, repo)
-
-	notif.NotifyOnSignal(syscall.SIGINT, os.Interrupt)
-	log.Println("Shutdown complete")
+	bootstrap.RunApi()
 }
