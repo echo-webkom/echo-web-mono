af5c4bb77aa35f6e4519e21162b7466d0c08e34f
jesperkha <hammerjesp@gmail.com>
2025-10-29 17:23:52 +0100

    Add pgxpool

--- apps/uno/data/repo/query.go
+++ apps/uno/data/repo/query.go
@@ -1,7 +1,44 @@
 package repo
 
-import "uno/data/model"
+import (
+	"context"
+	"uno/data/model"
+)
 
-func (r *Repo) GetAllHappenings() ([]model.Happening, error) {
-	return []model.Happening{}, nil
+type Row interface {
+	Scan(dest ...any) error
+}
+
+func scanHappening(row Row, hap *model.Happening) error {
+	return row.Scan(
+		&hap.ID,
+		&hap.Slug,
+		&hap.Title,
+		&hap.Type,
+		&hap.Date,
+		&hap.RegistrationGroups,
+		&hap.RegistrationStartGroups,
+		&hap.RegistrationStart,
+		&hap.RegistrationEnd,
+	)
+}
+
+func (r *Repo) GetAllHappenings(ctx context.Context) (res []model.Happening, err error) {
+	query := "SELECT * FROM happening"
+
+	rows, err := r.pool.Query(ctx, query)
+	if err != nil {
+		return res, err
+	}
+
+	for rows.Next() {
+		var hap model.Happening
+		if err := scanHappening(rows, &hap); err != nil {
+			return res, err
+		}
+
+		res = append(res, hap)
+	}
+
+	return res, err
 }
