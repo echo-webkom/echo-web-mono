623e105645b1e0cbda611945144cfec8b86a64b7
Ole Magnus Fon Johnsen <me@omfj.no>
2025-11-05 23:45:08 +0100

    test: more tests!!!

---
+++ apps/uno/domain/services/access_request_test.go
@@ -0,0 +1,25 @@
+package services_test
+
+import (
+	"testing"
+	"uno/domain/ports/mocks"
+	"uno/domain/services"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccessRequestService_Queries(t *testing.T) {
+	mockRepo := mocks.NewAccessRequestRepo(t)
+	accessRequestService := services.NewAccessRequestService(mockRepo)
+
+	accessRequestRepo := accessRequestService.Queries()
+	assert.NotNil(t, accessRequestRepo, "Expected AccessRequestRepo to be non-nil")
+}
+
+func TestAccessRequestService_AccessRequestRepo(t *testing.T) {
+	mockRepo := mocks.NewAccessRequestRepo(t)
+	accessRequestService := services.NewAccessRequestService(mockRepo)
+
+	accessRequestRepo := accessRequestService.AccessRequestRepo()
+	assert.NotNil(t, accessRequestRepo, "Expected AccessRequestRepo to be non-nil")
+}
