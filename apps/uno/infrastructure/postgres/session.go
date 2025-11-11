d3099ae5e87380f2c01bd7b3110f045d26ccb756
Ole Magnus Fon Johnsen <me@omfj.no>
2025-11-11 10:51:45 +0100

    feat: more domain models

--- apps/uno/infrastructure/postgres/session.go
+++ apps/uno/infrastructure/postgres/session.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"uno/domain/model"
 	"uno/domain/ports"
+	"uno/infrastructure/postgres/models"
 )
 
 type PostgresSessionImpl struct {
@@ -16,5 +17,20 @@ func NewSessionRepo(db *Database, logger ports.Logger) ports.SessionRepo {
 }
 
 func (r *PostgresSessionImpl) GetSessionByToken(ctx context.Context, token string) (model.Session, error) {
-	return model.Session{}, nil
+	r.logger.Info(ctx, "getting session by token")
+
+	query := `--sql
+		SELECT session_token, user_id, expires
+		FROM session
+		WHERE session_token = $1
+	`
+	var sessionDB models.SessionDB
+	err := r.db.GetContext(ctx, &sessionDB, query, token)
+	if err != nil {
+		r.logger.Error(ctx, "failed to get session by token",
+			"error", err,
+		)
+		return model.Session{}, err
+	}
+	return *sessionDB.ToDomain(), nil
 }
