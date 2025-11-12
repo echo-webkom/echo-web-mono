ef21283520e1a91d10b0c683eb21f60e8b0413b7
mikaeov <mikaeov@ntnu.no>
2025-11-12 19:18:05 +0100

    finpuss

--- apps/web/src/app/(default)/hjem/_components/cookies-banner.tsx
+++ apps/web/src/app/(default)/hjem/_components/cookies-banner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
@@ -18,14 +18,6 @@ export default function Cookies() {
     setConsent("Accepted");
   };
 
-  useEffect(() => {
-    if (consent !== "Accepted") {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  }, [consent]);
-
   if (!isMounted) {
     return null;
   }
@@ -33,15 +25,16 @@ export default function Cookies() {
   if (consent !== "Accepted") {
     return (
       <div>
-        <div className="absolute inset-0 z-49 overscroll-none bg-black/50 backdrop-blur-sm" />
-
-        <div className="fixed bottom-10 z-50 flex justify-self-center rounded-2xl border-2 border-black bg-white p-5 text-black sm:max-w-1/2">
+        <div className="fixed bottom-10 z-50 flex w-full justify-self-center rounded-2xl border-2 border-black bg-white p-5 text-black sm:max-w-1/2">
           <div className="space-y-5">
             <h1 className="text-2xl font-bold">Vi bruker cookies!</h1>
             <p>
               For å sikre kvaliteten på tjenestene våre, og for å minne alle på at echo alltid
               skrives med liten e, bruker vi cookies 🍪.
-              <Link href="/informasjonskapsler"> Les mer ➡️</Link>
+              <Link className="underline" href="/informasjonskapsler">
+                {" "}
+                Les mer ➡️
+              </Link>
             </p>
             <Button
               size="lg"
@@ -49,7 +42,7 @@ export default function Cookies() {
               variant="outline"
               className="bg-amber-100 text-black"
             >
-              Godta
+              Forstått
             </Button>
           </div>
         </div>
