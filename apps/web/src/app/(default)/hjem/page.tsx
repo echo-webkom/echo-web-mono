a5261c76ff364871cbde9b014542b08f23203ea1
Ole Magnus <me@omfj.no>
2025-11-17 12:37:08 +0100

    feat: add aoc leaderboard (#2739)

--- apps/web/src/app/(default)/hjem/page.tsx
+++ apps/web/src/app/(default)/hjem/page.tsx
@@ -1,3 +1,4 @@
+import { AocLeaderboard } from "@/components/aoc-leaderboard";
 import { Container } from "@/components/container";
 import { ensureUser } from "@/lib/ensure";
 import { Banner } from "./_components/banner";
@@ -35,13 +36,7 @@ export default async function Home() {
             n={11}
             className="col-span-1 row-span-2"
           />
-          <ComingHappenings
-            title="Bedriftspresentasjoner"
-            href="/for-studenter/arrangementer?type=bedpres"
-            types={["bedpres"]}
-            n={3}
-            className="col-span-2 row-span-1"
-          />
+          <AocLeaderboard className="col-span-2 row-span-1" />
           <Posts className="col-span-2 row-span-1" />
         </Container>
 
--- apps/web/src/components/aoc-leaderboard.tsx
+++ apps/web/src/components/aoc-leaderboard.tsx
@@ -1,12 +1,24 @@
-import { fetchAocLeaderboard, mapAocLeaderboard } from "@/data/advent-of-code/leaderboard";
+import { BentoBox } from "@/app/(default)/hjem/_components/bento-box";
+import {
+  fetchAocLeaderboard,
+  LEADERBOARD_ID,
+  LEADERBOARD_URL,
+  mapAocLeaderboard,
+  YEAR,
+} from "@/data/advent-of-code/leaderboard";
 import { cn } from "@/utils/cn";
+import { AocCountdownOverlay } from "./aoc-countdown-overlay";
 
 /**
  * Display the leaderboard for echo's private Advent of Code leaderboard.
  * Make sure that the environment variable `AOC_SESSION_COOKIE` is set, or
  * else it won't be able to fetch the leaderobard.
  */
-export const AocLeaderboard = async () => {
+type AocLeaderboardProps = {
+  className?: string;
+};
+
+export const AocLeaderboard = async ({ className }: AocLeaderboardProps) => {
   const leaderboard = await fetchAocLeaderboard();
 
   if (!leaderboard) {
@@ -17,38 +29,57 @@ export const AocLeaderboard = async () => {
     );
   }
 
-  const list = mapAocLeaderboard(leaderboard).slice(0, 14);
+  const list = mapAocLeaderboard(leaderboard).slice(0, 13);
 
   return (
-    <div className="h-full rounded-xl bg-[#0f0f23] p-4 font-mono text-xs text-white sm:text-sm lg:text-base">
-      {list.map((user, i) => {
-        const isLongName = user.name.length > 8;
-        const name = isLongName ? user.name.slice(0, 5) + "..." : user.name;
-        return (
-          <div key={user.id} className="line-clamp-1 flex items-center gap-4 text-nowrap">
-            <span className="w-[30px] shrink-0 text-gray-500">{i + 1})</span>
-            <span className="block w-[60px] shrink-0 sm:hidden">{name}</span>
-            <span className="w-[40px] shrink-0">{user.localScore}</span>
-            <div className="flex shrink-0 items-center">
-              {Array.from({ length: 25 }).map((_, i) => {
-                const completed = user.days[i + 1];
-                return (
-                  <span
-                    key={i}
-                    className={cn("text-[#0f0f23]", {
-                      "text-yellow-400": completed === 2,
-                      "text-blue-700": completed === 1,
-                    })}
-                  >
-                    *
-                  </span>
-                );
-              })}
-            </div>
-            <span className="hidden shrink sm:block">{user.name}</span>
-          </div>
-        );
-      })}
-    </div>
+    <BentoBox
+      title={`Advent of Code ${YEAR}`}
+      href={LEADERBOARD_URL}
+      className={cn(
+        "relative h-full rounded-lg border-2 border-[#298a08] bg-[#0f0f23] p-4 font-mono text-xs text-white sm:text-sm lg:text-base",
+        className,
+      )}
+    >
+      <div className="relative h-full min-h-[400px]">
+        <AocCountdownOverlay joinUrl={`https://adventofcode.com/${YEAR}/leaderboard/private`} />
+        <p className="mb-4 text-gray-400">
+          Bli med ved å joine {LEADERBOARD_ID} her:{" "}
+          <a href="https://adventofcode.com/2025/leaderboard/private" className="underline">
+            https://adventofcode.com/2025/leaderboard/private
+          </a>
+        </p>
+
+        <ol>
+          {list.map((user, i) => {
+            const isLongName = user.name.length > 12;
+            const name = isLongName ? user.name.slice(0, 9) + "..." : user.name;
+            return (
+              <li key={user.id} className="line-clamp-1 flex items-center gap-4 text-nowrap">
+                <span className="w-[30px] shrink-0 text-gray-500">{i + 1})</span>
+                <span className="block w-[60px] shrink-0 sm:hidden">{name}</span>
+                <span className="w-[40px] shrink-0">{user.localScore}</span>
+                <div className="flex shrink-0 items-center">
+                  {Array.from({ length: 25 }).map((_, i) => {
+                    const completed = user.days[i + 1];
+                    return (
+                      <span
+                        key={i}
+                        className={cn("text-[#0f0f23]", {
+                          "text-yellow-400": completed === 2,
+                          "text-blue-700": completed === 1,
+                        })}
+                      >
+                        *
+                      </span>
+                    );
+                  })}
+                </div>
+                <span className="hidden shrink sm:block">{user.name}</span>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </BentoBox>
   );
 };
