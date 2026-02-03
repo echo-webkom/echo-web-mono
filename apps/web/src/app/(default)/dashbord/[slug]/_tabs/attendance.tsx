23683fb5e42a4d067aec3d44c6409dee136fdb6f
andrevdal <andreas.drevdal@gmail.com>
2026-02-03 17:04:58 +0100

    early implementation of qr scanner

--- apps/web/src/app/(default)/dashbord/[slug]/_tabs/attendance.tsx
+++ apps/web/src/app/(default)/dashbord/[slug]/_tabs/attendance.tsx
@@ -1,7 +1,28 @@
-export const AttendanceTab = () => {
+import { getStudentGroups } from "@/data/groups/queries";
+import { getFullHappening } from "@/data/happenings/queries";
+import { QrScanner } from "../_components/qr-scanner";
+import { RegistrationTable } from "../_components/registration-table";
+import { RegistrationWithUser } from "../_lib/types";
+
+type RegistrationsTabProps = {
+  happening: Exclude<Awaited<ReturnType<typeof getFullHappening>>, undefined>;
+  registrations: Array<RegistrationWithUser>;
+};
+
+const groups = await getStudentGroups();
+export const AttendanceTab = ({ happening, registrations }: RegistrationsTabProps) => {
   return (
     <div>
-      <p>Hello world</p>
+      <h1>qr scanner</h1>
+      <QrScanner />
+      <RegistrationTable
+        questions={happening.questions}
+        registrations={registrations}
+        studentGroups={groups}
+        slug={happening.slug}
+        isBedpres={happening.type === "bedpres"}
+        happeningDate={happening.date}
+      />
     </div>
   );
 };
