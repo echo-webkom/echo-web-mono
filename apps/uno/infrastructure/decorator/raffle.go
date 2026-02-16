c4ceb2174f9eaaa548ebdd7a43e37ad859fd72b3
jesperkha <hammerjesp@gmail.com>
2026-02-16 12:17:51 +0100

    New raffle goroutine

--- apps/uno/infrastructure/decorator/raffle.go
+++ apps/uno/infrastructure/decorator/raffle.go
@@ -2,7 +2,6 @@ package decorator
 
 import (
 	"context"
-	"errors"
 	"sync"
 	"time"
 	"uno/domain/model"
@@ -17,10 +16,11 @@ import (
 // TODO: make /happenings/{id}/registrations/{id}/status endpoint
 
 const (
-	// How long the raffle period lasts at the start of a bedpres registration period
-	RAFFLE_DURATION = time.Minute * 1
 	// Maximum length of registration queue before blocking
 	MAX_QUEUE = 200
+
+	// How long the raffle period lasts at the start of a bedpres registration period
+	RAFFLE_DURATION = 10 * time.Second
 )
 
 // RaffleDecorator wraps port.RegistrationRepo and adds raffle
@@ -33,28 +33,11 @@ type RaffleDecorator struct {
 }
 
 type eventQueue struct {
-	windowClose time.Time
-	queue       chan registration
+	queue chan registration
 }
 
 type registration struct {
-	ctx              context.Context
-	userID           string
-	happening        model.Happening
-	spotRanges       []model.SpotRange
-	hostGroups       []string
-	canSkipSpotRange bool
-}
-
-func newRegistration(
-	ctx context.Context,
-	userID string,
-	happening model.Happening,
-	spotRanges []model.SpotRange,
-	hostGroups []string,
-	canSkipSpotRange bool,
-) registration {
-	return registration{ctx, userID, happening, spotRanges, hostGroups, canSkipSpotRange}
+	regId int
 }
 
 func NewRaffleDecorator(repo port.RegistrationRepo, notif *notifier.Notifier) port.RegistrationRepo {
@@ -80,70 +63,66 @@ func (r *RaffleDecorator) CreateRegistration(
 		return r.repo.CreateRegistration(ctx, userID, happening, spotRanges, hostGroups, canSkipSpotRange)
 	}
 
-	// This lock is also held by r.flushQueue, ensuring that the queue
+	// This lock is also held by r.flushAndDeleteQueue, ensuring that the queue
 	// is flushed before any new registrations have the chance to go through.
 	r.mu.Lock()
+	defer r.mu.Unlock()
 
 	// SAFETY: asserted not nil
 	windowClose := (*happening.RegistrationStart).Add(RAFFLE_DURATION)
 
-	// If raffle window is still open, get or create event queue and append the registrations
-	if time.Now().Before(windowClose) {
-		eq, ok := r.queues[happening.ID]
-		if !ok {
-			eq = &eventQueue{
-				windowClose: windowClose,
-				queue:       make(chan registration, MAX_QUEUE),
-			}
-			r.queues[happening.ID] = eq
-		}
-		r.mu.Unlock()
-
-		eq.queue <- newRegistration(ctx, userID, happening, spotRanges, hostGroups, canSkipSpotRange)
-		return &model.Registration{
-			UserID:           userID,
-			HappeningID:      happening.ID,
-			Status:           model.RegistrationStatusPending,
-			UnregisterReason: nil,
-			CreatedAt:        time.Now(),
-			PrevStatus:       nil,
-			ChangedAt:        nil,
-			ChangedBy:        nil,
-		}, false, nil
+	// If raffle window is closed, register normally without enqueuing.
+	if time.Now().After(windowClose) {
+		return r.repo.CreateRegistration(ctx, userID, happening, spotRanges, hostGroups, canSkipSpotRange)
 	}
 
-	// Otherwise the window has closed and we need to empty the queue before registering
-	r.mu.Unlock()
-	err1 := r.flushQueue(happening.ID)
-	reg, waitlisted, err2 := r.repo.CreateRegistration(ctx, userID, happening, spotRanges, hostGroups, canSkipSpotRange)
+	eq, ok := r.queues[happening.ID]
+	if !ok {
+		eq = &eventQueue{
+			queue: make(chan registration, MAX_QUEUE),
+		}
 
-	return reg, waitlisted, errors.Join(err1, err2)
+		go func() {
+			<-time.After(time.Until(windowClose))
+			_ = r.flushAndDeleteQueue(happening.ID)
+		}()
+
+		r.queues[happening.ID] = eq
+	}
+
+	eq.queue <- registration{
+		regId: 0, // TODO: add regid
+	}
+
+	return &model.Registration{
+		UserID:           userID,
+		HappeningID:      happening.ID,
+		Status:           model.RegistrationStatusPending,
+		UnregisterReason: nil,
+		CreatedAt:        time.Now(),
+		PrevStatus:       nil,
+		ChangedAt:        nil,
+		ChangedBy:        nil,
+	}, false, nil
 }
 
-func (r *RaffleDecorator) flushQueue(happeningId string) error {
+func (r *RaffleDecorator) flushAndDeleteQueue(happeningId string) error {
 	// Lock any new incomming registrations for this
 	// happening from trying to enqueue or register.
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	eq, ok := r.queues[happeningId]
+	_, ok := r.queues[happeningId]
 
 	// No queue to flush
 	if !ok {
 		return nil
 	}
 
-	var errs error
-	for {
-		select {
-		case reg := <-eq.queue:
-			_, _, err := r.repo.CreateRegistration(reg.ctx, reg.userID, reg.happening, reg.spotRanges, reg.hostGroups, reg.canSkipSpotRange)
-			errs = errors.Join(errs, err)
-		default:
-			delete(r.queues, happeningId) // SAFETY: locked
-			return errs
-		}
-	}
+	// TODO: randomize and flush queue.
+
+	delete(r.queues, happeningId) // SAFETY: locked
+	return nil
 }
 
 // GetByUserAndHappening implements port.RegistrationRepo.
