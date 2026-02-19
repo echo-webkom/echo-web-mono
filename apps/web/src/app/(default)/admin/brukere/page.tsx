24849ced84eb62346b26f2213ff7b6fc6f5aa6b8
Ole Magnus Fon Johnsen <me@omfj.no>
2026-02-19 03:16:16 +0100

    feat: use users endpoint

--- apps/web/src/app/(default)/admin/brukere/page.tsx
+++ apps/web/src/app/(default)/admin/brukere/page.tsx
@@ -1,16 +1,17 @@
 import { db } from "@echo-webkom/db/serverless";
 
-import { getAllUsers } from "@/data/users/queries";
+import { unoWithAdmin } from "@/api/server";
+import { type UnoClientType } from "@/api/uno/client";
 import { ensureWebkom } from "@/lib/ensure";
 import { UserTableView } from "./user-table";
 
 export const dynamic = "force-dynamic";
 
-export type AllUsers = Awaited<ReturnType<typeof getAllUsers>>;
+export type AllUsers = Awaited<ReturnType<UnoClientType["users"]["all"]>>;
 
 export default async function UsersOverview() {
   await ensureWebkom();
+  const [users, groups] = await Promise.all([unoWithAdmin.users.all(), db.query.groups.findMany()]);
 
-  const [users, groups] = await Promise.all([getAllUsers(), db.query.groups.findMany()]);
   return <UserTableView users={users} groups={groups} />;
 }
