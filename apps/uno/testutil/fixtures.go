1de354740375cb01e8f4eb0edd822a4a8359880b
Ole Magnus Fon Johnsen <me@omfj.no>
2026-02-20 18:39:48 +0100

    feat: add profile pic endpoints

--- apps/uno/testutil/fixtures.go
+++ apps/uno/testutil/fixtures.go
@@ -8,6 +8,10 @@ import (
 	"github.com/brianvoe/gofakeit/v7"
 )
 
+const (
+	FakeApiURL = "http://test"
+)
+
 func NewFakeStruct[T any](overrides ...func(*T)) T {
 	var obj T
 	_ = gofakeit.Struct(&obj)
