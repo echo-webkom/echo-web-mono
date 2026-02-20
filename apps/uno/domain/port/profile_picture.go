a6edee1ce8b5eb681ac29ee0c694575e208e34e6
Ole Magnus Fon Johnsen <me@omfj.no>
2026-02-20 18:33:01 +0100

    feat: add profile pic repo

---
+++ apps/uno/domain/port/profile_picture.go
@@ -0,0 +1,12 @@
+package port
+
+import (
+	"context"
+	"uno/domain/model"
+)
+
+type ProfilePictureRepo interface {
+	UploadProfilePicture(ctx context.Context, userID string, picture *model.ProfilePictureUpload) error
+	DeleteProfilePicture(ctx context.Context, userID string) error
+	GetProfilePicture(ctx context.Context, userID string) (*model.ProfilePicture, error)
+}
