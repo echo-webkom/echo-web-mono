7754ce735f3a8fe2f98a429429242ece4d6cc5b2
Ole Magnus Fon Johnsen <me@omfj.no>
2026-02-21 00:55:16 +0100

    feat: make fail silently when no file storage

--- apps/uno/domain/service/user.go
+++ apps/uno/domain/service/user.go
@@ -2,11 +2,14 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 	"uno/domain/model"
 	"uno/domain/port"
 )
+
+var ErrFileStorageNotConfigured = errors.New("file storage not configured")
 
 type UserService struct {
 	apiURL             string
@@ -39,7 +42,18 @@ func (s *UserService) GetUsersWithBirthdayToday(ctx context.Context) ([]model.Us
 	return s.userRepo.GetUsersWithBirthday(ctx, time.Now().In(norway))
 }
 
+func (s *UserService) GetProfilePicture(ctx context.Context, userID string) (*model.ProfilePicture, error) {
+	if s.profilePictureRepo == nil {
+		return nil, ErrFileStorageNotConfigured
+	}
+	return s.profilePictureRepo.GetProfilePicture(ctx, userID)
+}
+
 func (s *UserService) DeleteUserImage(ctx context.Context, userID string) error {
+	if s.profilePictureRepo == nil {
+		return ErrFileStorageNotConfigured
+	}
+
 	user, err := s.userRepo.GetUserByID(ctx, userID)
 	if err != nil {
 		return err
@@ -53,6 +67,10 @@ func (s *UserService) DeleteUserImage(ctx context.Context, userID string) error 
 }
 
 func (s *UserService) UploadProfileImage(ctx context.Context, userID string, profilePicture *model.ProfilePictureUpload) (string, error) {
+	if s.profilePictureRepo == nil {
+		return "", ErrFileStorageNotConfigured
+	}
+
 	err := profilePicture.Validate()
 	if err != nil {
 		return "", err
