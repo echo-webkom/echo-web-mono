bb30f8e0f4c6142f0897c8d1dba02e68cf8eaed3
andrevdal <andreas.drevdal@gmail.com>
2026-02-27 12:48:58 +0100

    making table under header in registration table subcomponent so table can be used seperatly

--- apps/web/src/app/(default)/dashbord/[slug]/_components/qr-scanner.tsx
+++ apps/web/src/app/(default)/dashbord/[slug]/_components/qr-scanner.tsx
@@ -9,6 +9,7 @@ import { RegistrationWithUser } from "../_lib/types";
 import { useRegistrationFilter } from "../_lib/use-registration-filter";
 import { Table, TableHead, TableRow } from "../../../../../components/ui/table";
 import { getFullHappening } from "../../../../../data/happenings/queries";
+import { RegistrationList } from "./registration-list";
 import { RegistrationRow } from "./registration-row";
 import { RegistrationTable } from "./registration-table";
 
@@ -66,58 +67,22 @@ export const QrScanner = ({ registrations, happening, studentGroups }: QrScanner
         ))}
       </ul>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            {showIndex && (
-              <TableHead scope="col" className="w-12">
-                #
-              </TableHead>
-            )}
-            <TableHead scope="col" className="w-12">
-              Info
-            </TableHead>
-            <TableHead scope="col">Navn</TableHead>
-            <TableHead scope="col">Status</TableHead>
-            <TableHead scope="col" className="w-16">
-              Mer
-            </TableHead>
-            <TableHead scope="col" className="w-12">
-              {/* Empty */}
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {filteredRegistrations.length === 0 && (
-            <TableRow>
-              <td colSpan={showIndex ? 6 : 5}>
-                <p className="text-muted-foreground py-6 text-center text-xl font-medium">
-                  Ingen resultater
-                </p>
-              </td>
-            </TableRow>
-          )}
-          {filteredRegistrations.map((registration, i) => (
-            <RegistrationRow
-              key={registration.user.id}
-              index={i}
-              registration={registration}
-              showIndex={showIndex}
-              isBedpres={happening.type === "bedpres"}
-              happeningDate={happening.date}
-            />
-          ))}
-        </TableBody>
-      </Table>
-
-      <RegistrationTable
-        questions={happening.questions}
+      <RegistrationList
         registrations={registrations}
         studentGroups={studentGroups}
         slug={happening.slug}
         isBedpres={happening.type === "bedpres"}
         happeningDate={happening.date}
       />
+
+      {/* <RegistrationTable
+        questions={happening.questions}
+        registrations={registrations}
+        studentGroups={studentGroups}
+        slug={happening.slug}
+        isBedpres={happening.type === "bedpres"}
+        happeningDate={happening.date}
+      /> */}
     </>
   );
 };
