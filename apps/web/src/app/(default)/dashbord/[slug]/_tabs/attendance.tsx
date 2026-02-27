992ad5a38c19da882de55e3c5d97c0290afb6622
andrevdal <andreas.drevdal@gmail.com>
2026-02-27 12:48:58 +0100

    making table under header in registration table subcomponent so table can be used seperatly

--- apps/web/src/app/(default)/dashbord/[slug]/_tabs/attendance.tsx
+++ apps/web/src/app/(default)/dashbord/[slug]/_tabs/attendance.tsx
@@ -1,15 +1,15 @@
-import { getStudentGroups } from "@/data/groups/queries";
 import { getFullHappening } from "@/data/happenings/queries";
 import { QrScanner } from "../_components/qr-scanner";
 import { RegistrationTable } from "../_components/registration-table";
 import { RegistrationWithUser } from "../_lib/types";
+import { getStudentGroupsWithMembers } from "../../../../../data/groups/queries";
 
 type RegistrationsTabProps = {
   happening: Exclude<Awaited<ReturnType<typeof getFullHappening>>, undefined>;
   registrations: Array<RegistrationWithUser>;
 };
 
-const groups = await getStudentGroups();
+const groups = await getStudentGroupsWithMembers();
 export const AttendanceTab = ({ happening, registrations }: RegistrationsTabProps) => {
   return (
     <div>
