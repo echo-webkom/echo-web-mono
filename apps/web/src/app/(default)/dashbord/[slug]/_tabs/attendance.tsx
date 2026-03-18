46e06e127ecd1270a324ab07d70e6fa5c5cada62
andrevdal <andreas.drevdal@gmail.com>
2026-03-18 17:32:11 +0100

    fixx web build

--- apps/web/src/app/(default)/dashbord/[slug]/_tabs/attendance.tsx
+++ apps/web/src/app/(default)/dashbord/[slug]/_tabs/attendance.tsx
@@ -1,3 +1,4 @@
+import { unoWithAdmin } from "@/api/server";
 import { type FullHappening, type Group } from "@/api/uno/client";
 import { QrScanner } from "../_components/qr-scanner";
 import { RegistrationWithUser } from "../_lib/types";
@@ -5,12 +6,13 @@ import { RegistrationWithUser } from "../_lib/types";
 type RegistrationsTabProps = {
   happening: FullHappening;
   registrations: Array<RegistrationWithUser>;
-  groups: Array<Group>;
 };
 
-export const AttendanceTab = ({ happening, registrations, groups }: RegistrationsTabProps) => {
+export const AttendanceTab = async ({ happening, registrations }: RegistrationsTabProps) => {
+  const groups = await unoWithAdmin.groups.all();
   return (
     <div>
+      <h1>qr scanner</h1>
       <QrScanner registrations={registrations} happening={happening} studentGroups={groups} />
     </div>
   );
