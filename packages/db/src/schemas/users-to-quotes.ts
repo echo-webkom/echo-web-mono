fcb6054d34caa617d5847928f8515349df148c2c
Ole Magnus Fon Johnsen <me@omfj.no>
2026-03-19 14:11:30 +0100

    chore: add foreign key constraint to users_to_quotes table

--- packages/db/src/schemas/users-to-quotes.ts
+++ packages/db/src/schemas/users-to-quotes.ts
@@ -2,11 +2,17 @@ import { type InferInsertModel, type InferSelectModel } from "drizzle-orm";
 import { pgTable, primaryKey, text } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
+import { quotes } from "./quotes";
+
 export const usersToQuotes = pgTable(
   "users_to_quotes",
   {
     userId: text("user_id").notNull(),
-    quoteId: text("quote_id").notNull(),
+    quoteId: text("quote_id")
+      .notNull()
+      .references(() => quotes.id, {
+        onDelete: "cascade",
+      }),
     reactionType: text("reaction_type").notNull(),
   },
   (t) => [primaryKey({ columns: [t.userId, t.quoteId] })],
