60c513fa033a3876ba601b562a4770343a8c2ba3
jesperkha <hammerjesp@gmail.com>
2026-04-10 13:31:41 +0200

    Rm pnpm check

--- check.sh
+++ check.sh
@@ -19,7 +19,6 @@ check "air"                  "command -v air"                                   
 check "swag"                 "command -v swag"                                  "swag is not installed (go install github.com/swaggo/swag/cmd/swag@latest)"
 check "mockery"              "command -v mockery"                               "mockery is not installed (go install github.com/vektra/mockery/v3@latest)"
 check "golangci-lint"        "command -v golangci-lint"                         "golangci-lint is not installed"
-check "node_modules"         "pnpm install --frozen-lockfile --lockfile-only"   "missing dependencies (pnpm i)"
 check "cenv configuration"   "cenv check"                                       "cenv check failed (cenv fix)"
 
 [ $FAILED -eq 0 ] && echo "All checks passed." || { echo "Some checks failed."; exit 1; }
