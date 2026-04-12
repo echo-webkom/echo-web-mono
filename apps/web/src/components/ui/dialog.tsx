f46ba35c49f599af0b7ca901f977f495053ec108
Ole Magnus Fon Johnsen <me@omfj.no>
2026-04-12 20:33:12 +0200

    fix: dialog overflow

--- apps/web/src/components/ui/dialog.tsx
+++ apps/web/src/components/ui/dialog.tsx
@@ -54,7 +54,7 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 overflow-hidden border shadow-lg duration-200 sm:rounded-lg",
+          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 w-full max-w-[min(90vw,32rem)] max-h-[90vh] translate-x-[-50%] translate-y-[-50%] overflow-y-auto border shadow-lg duration-200 rounded-md",
           className,
         )}
         {...props}
@@ -78,7 +78,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="dialog-header"
       className={cn(
-        "bg-table-background-alt flex flex-col space-y-1.5 p-6 text-center sm:text-left",
+        "bg-table-background-alt sticky top-0 z-10 flex flex-col space-y-1.5 p-6 text-center sm:text-left",
         className,
       )}
       {...props}
@@ -97,7 +97,10 @@ function DialogFooter({
   return (
     <div
       data-slot="dialog-footer"
-      className={cn("flex flex-col-reverse p-6 sm:flex-row sm:justify-end sm:space-x-2", className)}
+      className={cn(
+        "sticky bottom-0 flex flex-col-reverse p-3 sm:flex-row sm:justify-end sm:space-x-2",
+        className,
+      )}
       {...props}
     >
       {children}
@@ -138,7 +141,7 @@ function DialogDescription({
 
 function DialogBody({ className, children, ...props }: React.ComponentProps<"div">) {
   return (
-    <div className="max-h-[60vh] overflow-y-auto" {...props}>
+    <div {...props}>
       <div className={cn("px-6 py-3", className)}>{children}</div>
     </div>
   );
@@ -146,7 +149,7 @@ function DialogBody({ className, children, ...props }: React.ComponentProps<"div
 
 function DialogBodyOverflow({ className, children, ...props }: React.ComponentProps<"div">) {
   return (
-    <div className="max-h-[60vh] overflow-y-visible" {...props}>
+    <div {...props}>
       <div className={cn("px-6 py-3", className)}>{children}</div>
     </div>
   );
