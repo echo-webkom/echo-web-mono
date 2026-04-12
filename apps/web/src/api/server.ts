34ed7bf6e952b6d31c20df14e5840cf6817bc9bb
Ole Magnus Fon Johnsen <me@omfj.no>
2026-04-12 17:09:35 +0200

    feat: add support for internal API URL

--- apps/web/src/api/server.ts
+++ apps/web/src/api/server.ts
@@ -2,6 +2,6 @@ import "server-only";
 import { UnoClient } from "./uno/client";
 
 export const unoWithAdmin = new UnoClient({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  baseUrl: process.env.INTERNAL_API_URL ?? process.env.NEXT_PUBLIC_API_URL,
   adminToken: process.env.ADMIN_KEY,
 });
